41f3e2ea6579bdd5326d417d0fbec0d50a6ca85a
Yifeng Wang <i@ewind.us>
2018-04-02 10:38:10 +0800

    feat: add quadraticCurveTo demo

--- scalpel/src/index.js
+++ scalpel/src/index.js
@@ -17,29 +17,48 @@ export class Scalpel {
       const box = canvas.getBoundingClientRect()
       this.state.mouseX = (e.clientX - box.left)
       this.state.mouseY = (e.clientY - box.top)
-      this.render()
+      this.draw()
     })
 
     canvas.addEventListener('click', (e) => {
       const box = canvas.getBoundingClientRect()
-      const pointX = (e.clientX - box.left)
-      const pointY = (e.clientY - box.top)
-      this.points.push([pointX, pointY])
+      const x = (e.clientX - box.left)
+      const y = (e.clientY - box.top)
+      this.points.push({ x, y })
     })
   }
-
-  render = () => {
-    const { ctx, canvas, state } = this
+  // Basic `quadraticCurveTo` approach.
+  draw = () => {
+    const { ctx, canvas, state, points } = this
     const { mouseX, mouseY } = state
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     ctx.fillStyle = 'red'
-    this.points.forEach(([x, y]) => {
+    points.forEach(({ x, y }) => {
       ctx.fillRect(x, y, 5, 5)
     })
 
     ctx.fillStyle = 'black'
     ctx.fillRect(mouseX, mouseY, 5, 5)
+
+    if (points.length <= 4) return
+
+    ctx.beginPath()
+    ctx.strokeStyle = 'blue'
+    ctx.moveTo(points[0].x, points[0].y)
+
+    let i
+    for (i = 1; i < points.length - 1; i++) {
+      const xMid = (points[i].x + points[i + 1].x) / 2
+      const yMid = (points[i].y + points[i + 1].y) / 2
+      const cpX1 = (xMid + points[i].x) / 2
+      // const cpY1 = (yMid + points[i].y) / 2
+      const cpX2 = (xMid + points[i + 1].x) / 2
+      // const cpY2 = (yMid + points[i + 1].y) / 2
+      ctx.quadraticCurveTo(cpX1, points[i].y, xMid, yMid)
+      ctx.quadraticCurveTo(cpX2, points[i + 1].y, points[i + 1].x, points[i + 1].y)
+    }
+    ctx.stroke()
   }
 }
