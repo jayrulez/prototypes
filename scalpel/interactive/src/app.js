e2ee55cb139b7b6c4dbe2ebcc30eeae857ab8160
Yifeng Wang <i@ewind.us>
2018-04-03 15:31:30 +0800

    feat: add hit func

--- scalpel/interactive/src/app.js
+++ scalpel/interactive/src/app.js
@@ -18,11 +18,11 @@ export class App {
       this.points.push(x, y)
       // https://github.com/konvajs/konva/issues/378
       this.line.points(this.points.concat())
-      this.ininCircle(x, y)
+      this.initCircle(x, y)
     })
   }
 
-  ininCircle (x, y) {
+  initCircle (x, y) {
     const circle = new Circle({
       x,
       y,
@@ -30,7 +30,16 @@ export class App {
       id: `circle-${this.points.length / 2}`,
       fill: 'red',
       stroke: 'black',
-      draggable: true
+      draggable: true,
+      hitFunc: function (context) {
+        context.beginPath()
+        context.arc(0, 0, 15, 0, Math.PI * 2, true)
+        context.closePath()
+        context.fillStrokeShape(circle)
+      }
+    })
+    circle.on('mouseenter', () => {
+      console.log('enter')
     })
     circle.on('dragmove', () => {
       this.getNewPoints(circle, this.points)
