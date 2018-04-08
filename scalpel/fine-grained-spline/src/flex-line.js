144d00da026e97078cccceba1aec795f9f8fb43d
Yifeng Wang <i@ewind.us>
2018-04-08 13:41:37 +0800

    refactor: split flex line class

--- scalpel/fine-grained-spline/src/flex-line.js
+++ scalpel/fine-grained-spline/src/flex-line.js
@@ -1,3 +1,36 @@
+import {
+  initLine,
+  initCircle,
+  initTooltip
+} from './conf'
+
 export class FlexLine {
   points = []
+  line = initLine()
+  count = 0
+
+  constructor (app) {
+    this.app = app
+  }
+
+  get linePoints () {
+    return this.points
+      .map(({ circle }) => [circle.x(), circle.y()])
+      .reduce((a, b) => [...a, ...b], [])
+  }
+
+  addPoint = (x, y) => {
+    const id = this.count++
+
+    // Init circle and tooltip.
+    const circle = initCircle(x, y, id)
+    const tooltip = initTooltip(x, y, id)
+
+    this.points.push({ circle, tooltip })
+
+    this.app.layer.add(circle)
+    this.app.layer.add(tooltip)
+    // debugger // eslint-disable-line
+    this.app.layer.draw()
+  }
 }
--- scalpel/fine-grained-spline/src/index.js
+++ scalpel/fine-grained-spline/src/index.js
@@ -1,16 +1,3 @@
 import { App } from './app'
 
-const stageConf = {
-  container: 'container',
-  width: 640,
-  height: 480
-}
-const layerConf = {}
-const lineConf = {
-  stroke: '#666',
-  points: [],
-  bezier: true,
-  tension: 0.5
-}
-
-void new App(stageConf, layerConf, lineConf)
+void new App()
