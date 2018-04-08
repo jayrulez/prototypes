6833f3bc62649b46d795fa4452e8aafec90b4a3a
Yifeng Wang <i@ewind.us>
2018-04-08 19:13:52 +0800

    feat: handle custom begin quad curve

--- scalpel/fine-grained-spline/src/flex-line.js
+++ scalpel/fine-grained-spline/src/flex-line.js
@@ -1,5 +1,6 @@
-import { Line } from 'konva'
+import { Line, Shape } from 'konva'
 import { Point } from './point'
+import { getControlPoints } from './utils'
 
 export class FlexLine {
   points = []
@@ -14,11 +15,15 @@ export class FlexLine {
     if (len <= 1) return []
     const segments = []
     for (let i = 0; i < len - 1; i++) {
+      const isStart = i === 0
+      const isEnd = i + 1 === len - 1
       segments.push({
-        fromPoint: this.points[i],
-        toPoint: this.points[i + 1],
-        fromStart: i === 0,
-        toEnd: i + 1 === len - 1
+        from: this.points[i],
+        to: this.points[i + 1],
+        prev: isStart ? null : this.points[i - 1],
+        next: isEnd ? null : this.points[i + 2],
+        isStart,
+        isEnd
       })
     }
     return segments
@@ -31,21 +36,53 @@ export class FlexLine {
     point.draw()
   }
 
+  drawQuad = (segment) => {
+    const { from, to, next } = segment
+
+    // TODO downgrade to line when no next point exists.
+    if (!next) return
+    const helper = getControlPoints(
+      from.x,
+      from.y,
+      to.x,
+      to.y,
+      next.x,
+      next.y
+    )[0]
+
+    const line = new Shape({
+      sceneFunc: function (context) {
+        context.beginPath()
+        context.moveTo(from.x, from.y)
+        context.quadraticCurveTo(helper.x, helper.y, to.x, to.y)
+        context.closePath()
+        // Konva specific method.
+        context.strokeShape(this)
+      },
+      stroke: 'red',
+      strokeWidth: 1
+    })
+
+    const { layer } = this.app
+    layer.add(line)
+  }
+
   drawSegments = () => {
     this.lines.forEach(line => line.destroy())
     const { layer } = this.app
-    this.segments.forEach((segment, i) => {
-      const { fromStart, toEnd, fromPoint, toPoint } = segment
-      if (fromStart || toEnd) {
+    this.segments.forEach((segment) => {
+      const { isStart, isEnd, from, to } = segment
+      if (isStart || isEnd) {
         const line = new Line({
           stroke: '#666',
           points: [
-            fromPoint.x,
-            fromPoint.y,
-            toPoint.x,
-            toPoint.y
+            from.x,
+            from.y,
+            to.x,
+            to.y
           ]
         })
+        this.drawQuad(segment)
         this.lines.push(line)
         layer.add(line)
       }
--- scalpel/fine-grained-spline/src/utils.js
+++ scalpel/fine-grained-spline/src/utils.js
@@ -4,3 +4,17 @@ let _id = 0
 export function getId () {
   return _id++
 }
+
+export function getControlPoints (x0, y0, x1, y1, x2, y2, t = 0.5) {
+  const d01 = Math.sqrt(Math.pow(x1 - x0, 2) + Math.pow(y1 - y0, 2))
+  const d12 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
+  const fa = t * d01 / (d01 + d12)
+  const fb = t * d12 / (d01 + d12)
+  const p1x = x1 - fa * (x2 - x0)
+  const p1y = y1 - fa * (y2 - y0)
+  const p2x = x1 + fb * (x2 - x0)
+  const p2y = y1 + fb * (y2 - y0)
+  return [
+    { x: p1x, y: p1y }, { x: p2x, y: p2y }
+  ]
+}
