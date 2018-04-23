67a25b398dd93e2d1450438aecf0c38e074d026e
Yifeng Wang <i@ewind.us>
2018-04-23 11:11:48 +0800

    feat: split block and line tests

--- eslint-plugin-comment-pangu/tests/comment-test.js
+++ eslint-plugin-comment-pangu/tests/comment-test.js
@@ -5,17 +5,36 @@ const ruleTester = new RuleTester()
 ruleTester.run('comment', rule, {
   valid: [
     '// 123',
-    `/** demo
- * foo
- * bar
+    '//123',
+    '// 白色相簿 2',
+    '// 第一次, 有了喜欢的人',
+    '// 第一次，有了喜欢的人',
+    '// 为什么你会这么熟练啊！你和雪菜亲过多少次了啊！？',
+    `/* demo 测试 */`,
+    `/** demo 测试
  **/`
   ],
   invalid: [
     {
-      code: '// 这是123不合适的注释',
+      code: '// 白色相簿2',
       errors: [{
         message: 'Inappropriate Chinese spacing.',
         type: 'Line'
+      }]
+    },
+    {
+      code: `/* demo测试 */`,
+      errors: [{
+        message: 'Inappropriate Chinese spacing.',
+        type: 'Block'
+      }]
+    },
+    {
+      code: `/** demo测试
+      **/`,
+      errors: [{
+        message: 'Inappropriate Chinese spacing.',
+        type: 'Block'
       }]
     }
   ]
