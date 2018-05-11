14be9f17f4a3af7c8a4c15742d889338a26471cc
Yifeng Wang <i@ewind.us>
2018-05-11 17:35:01 +0800

    chore: add vue scaffold

--- svg-container/main.js
+++ svg-container/main.js
@@ -1,23 +1,24 @@
 import $ from 'jquery'
+import Vue from 'vue/dist/vue.js'
 
-function addE (offset) {
-
-}
-
-function subE (offset) {
-
-}
-
-function initUI () {
-  const btnAddE = document.getElementById('E+')
-  const btnSubE = document.getElementById('E-')
-  btnAddE.onclick = addE
-  btnSubE.onclick = subE
-}
-
-$(document).ready(() => {
-  initUI()
-
-  const svg = $('#demo')
-  window.svg = svg
+void new Vue({
+  el: '#app',
+  template: document.getElementById('template').innerHTML,
+  data () {
+    return {
+      message: 'Hello Vue!'
+    }
+  },
+  mounted () {
+    const svg = $('#demo')
+    window.svg = svg
+  },
+  methods: {
+    addE () {
+      console.log('addE TODO')
+    },
+    subE () {
+      console.log('subE TODO')
+    }
+  }
 })
