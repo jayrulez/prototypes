4813f7a6f465a80f34b68ce99fb91cf8a1aad7dc
Yifeng Wang <i@ewind.us>
2018-05-11 16:34:57 +0800

    chore: add demo scaffold

--- svg-container/main.js
+++ svg-container/main.js
@@ -1,6 +1,23 @@
 import $ from 'jquery'
 
+function addE (offset) {
+
+}
+
+function subE (offset) {
+
+}
+
+function initUI () {
+  const btnAddE = document.getElementById('E+')
+  const btnSubE = document.getElementById('E-')
+  btnAddE.onclick = addE
+  btnSubE.onclick = subE
+}
+
 $(document).ready(() => {
+  initUI()
+
   const svg = $('#demo')
   window.svg = svg
 })
