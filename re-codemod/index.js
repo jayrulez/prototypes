3dba4fbd810a606893468e77afe1831f9c576882
Yifeng Wang <i@ewind.us>
2018-06-13 12:57:33 +0800

    fix: re matcher

--- re-codemod/index.js
+++ re-codemod/index.js
@@ -3,28 +3,33 @@ const fs = require('fs-extra')
 
 const filePath = process.argv[2]
 
+// Usage
+// node index.js ../foo/\*.js
 glob(filePath, (err, files) => {
   if (err) throw err
 
   files.forEach(file => {
     fs.readFile(file, 'utf8').then(code => {
-      const re = /lodash\.(.)+\(/g
+      const re = /lodash\.(.)+?\(/g
       const matchResults = code.match(re)
+
+      if (!matchResults) return
+      console.log('mod', file)
 
       const methodNames = matchResults.map(
         result => result.replace('lodash.', '').replace('(', '')
       )
-      let modCode = code
-        .replace(
-          `import lodash from 'lodash'`,
-          `import { ${methodNames.join(', ')} } from 'lodash'`
-        )
+      const filteredNames = Array.from(new Set(methodNames))
+      let modCode = code.replace(
+        `import lodash from 'lodash'`,
+        `import { ${filteredNames.join(', ')} } from 'lodash'`
+      )
       matchResults.forEach((result, i) => {
         // eslint-disable-next-line
         const re = result.replace('.', '\\.').replace('(', '\\(')
         modCode = modCode.replace(new RegExp(re, 'g'), methodNames[i] + '(')
       })
-      console.log(modCode)
+      fs.writeFile(file, modCode, 'utf8')
     })
   })
 })
