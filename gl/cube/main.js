bbd7af040ff08615d6d29e6056bf90301399b890
Yifeng Wang <i@ewind.us>
2018-08-03 01:58:45 +0800

    perf: use static buffer

--- gl/cube/main.js
+++ gl/cube/main.js
@@ -1,8 +1,9 @@
-import { render } from './render'
+import { beforeRender, render } from './render'
 import { bufferGetter } from './buffers'
 import { initProgram } from './shaders'
 
 const gl = document.querySelector('#glcanvas').getContext('webgl')
 const programInfo = initProgram(gl)
 
-render(gl, programInfo, bufferGetter)
+const buffers = beforeRender(gl, programInfo, bufferGetter)
+render(gl, programInfo, buffers)
--- gl/cube/render.js
+++ gl/cube/render.js
@@ -2,15 +2,17 @@ import { matGetter } from './matrix'
 
 let delta = 0
 
-const beforeDraw = gl => {
-  gl.clearColor(0.0, 0.0, 0.0, 1.0)
-  gl.clearDepth(1.0)
-  gl.enable(gl.DEPTH_TEST)
-  gl.depthFunc(gl.LEQUAL)
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+export const beforeRender = (gl, programInfo, bufferGetter) => {
+  gl.useProgram(programInfo.program)
+
+  const buffers = []
+  for (let i = 0; i < 10; i++) {
+    buffers.push(bufferGetter(gl, Math.random() * 20 - 10))
+  }
+  return buffers
 }
 
-const draw = (gl, programInfo, mats, buffers) => {
+const draw = (gl, programInfo, mats, buffer) => {
   const { projectionMat, modelViewMat } = mats
   const { vertexPosition, vertexColor } = programInfo.attribLocations
   const { projectionMatrix, modelViewMatrix } = programInfo.uniformLocations
@@ -21,7 +23,7 @@ const draw = (gl, programInfo, mats, buffers) => {
     const normalize = false
     const stride = 0
     const offset = 0
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.positions)
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer.positions)
     gl.vertexAttribPointer(
       vertexPosition, numComponents, type, normalize, stride, offset
     )
@@ -34,19 +36,18 @@ const draw = (gl, programInfo, mats, buffers) => {
     const normalize = false
     const stride = 0
     const offset = 0
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.colors)
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer.colors)
     gl.vertexAttribPointer(
       vertexColor, numComponents, type, normalize, stride, offset
     )
     gl.enableVertexAttribArray(vertexColor)
   }
 
-  gl.useProgram(programInfo.program)
   gl.uniformMatrix4fv(projectionMatrix, false, projectionMat)
   gl.uniformMatrix4fv(modelViewMatrix, false, modelViewMat)
 
   {
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices)
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer.indices)
     const offset = 0
     const type = gl.UNSIGNED_SHORT
     const vertexCount = 36
@@ -54,18 +55,24 @@ const draw = (gl, programInfo, mats, buffers) => {
   }
 }
 
-const _render = (gl, programInfo, bufferGetter, delta) => {
+const _render = (gl, programInfo, buffers, delta) => {
   const mats = matGetter(gl, delta)
-  beforeDraw(gl)
-  draw(gl, programInfo, mats, bufferGetter(gl, -3))
-  draw(gl, programInfo, mats, bufferGetter(gl, 0))
-  draw(gl, programInfo, mats, bufferGetter(gl, 3))
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+  gl.clearDepth(1.0)
+  gl.enable(gl.DEPTH_TEST)
+  gl.depthFunc(gl.LEQUAL)
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+  for (let i = 0; i < buffers.length; i++) {
+    // TODO dynamic update buffers with extra transform.
+    draw(gl, programInfo, mats, buffers[i])
+  }
 }
 
-export const render = (gl, programInfo, bufferGetter) => {
+export const render = (gl, programInfo, buffers) => {
   window.requestAnimationFrame(() => {
     delta += 0.01
-    _render(gl, programInfo, bufferGetter, delta)
-    render(gl, programInfo, bufferGetter)
+    _render(gl, programInfo, buffers, delta)
+    render(gl, programInfo, buffers)
   })
 }
