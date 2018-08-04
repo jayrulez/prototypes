9b7fb1205fad6090491e765335796fa894e6b5bd
Yifeng Wang <i@ewind.us>
2018-08-04 11:09:47 +0800

    feat: add cube position getter

---
+++ gl/cube/cube.js
@@ -0,0 +1,57 @@
+const positionsFromCoords = (x, y, z) => {
+  const width = 1.9
+  const margin = 0.05
+  const basePositions = [
+    // Front face
+    -1.0, -1.0, 1.0,
+    1.0, -1.0, 1.0,
+    1.0, 1.0, 1.0,
+    -1.0, 1.0, 1.0,
+
+    // Back face
+    -1.0, -1.0, -1.0,
+    -1.0, 1.0, -1.0,
+    1.0, 1.0, -1.0,
+    1.0, -1.0, -1.0,
+
+    // Top face
+    -1.0, 1.0, -1.0,
+    -1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, -1.0,
+
+    // Bottom face
+    -1.0, -1.0, -1.0,
+    1.0, -1.0, -1.0,
+    1.0, -1.0, 1.0,
+    -1.0, -1.0, 1.0,
+
+    // Right face
+    1.0, -1.0, -1.0,
+    1.0, 1.0, -1.0,
+    1.0, 1.0, 1.0,
+    1.0, -1.0, 1.0,
+
+    // Left face
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0, 1.0,
+    -1.0, 1.0, 1.0,
+    -1.0, 1.0, -1.0
+  ]
+  const diff = width + 2 * margin
+  const positions = basePositions
+    .map((v, i) => v * width / 2 + [x, y, z][i % 3] * diff)
+  return positions
+}
+
+export const positionsGetter = () => {
+  const positons = []
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      for (let k = -1; k <= 1; k++) {
+        positons.push(positionsFromCoords(i, j, k))
+      }
+    }
+  }
+  return positons
+}
