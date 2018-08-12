6541986c85803f5165355af7c304568068ccf9c7
Yifeng Wang <i@ewind.us>
2018-08-12 14:11:33 +0800

    fix: rotation in different axis

--- gl/cube/cube.js
+++ gl/cube/cube.js
@@ -12,59 +12,59 @@ const COLORS = {
   GREY: [0.0, 0.0, 0.0, 0.5]
 }
 
-const [FRONT, BACK, TOP, BOTTOM, RIGHT, LEFT] = [0, 1, 2, 3, 4, 5]
+const [F, B, U, D, R, L] = [0, 1, 2, 3, 4, 5]
 const [NW, N, NE, W, C, E, SW, S, SE] = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 const FACE_MAPPING = {
   '-1': {
     '-1': {
-      '-1': [[BACK, SE], [BOTTOM, SW], [LEFT, SW]],
-      '0': [[BOTTOM, W], [LEFT, S]],
-      '1': [[FRONT, SW], [BOTTOM, NW], [LEFT, SE]]
+      '-1': [[B, SE], [D, SW], [L, SW]],
+      '0': [[D, W], [L, S]],
+      '1': [[F, SW], [D, NW], [L, SE]]
     },
     '0': {
-      '-1': [[BACK, E], [LEFT, W]],
-      '0': [[LEFT, C]],
-      '1': [[FRONT, W], [LEFT, E]]
+      '-1': [[B, E], [L, W]],
+      '0': [[L, C]],
+      '1': [[F, W], [L, E]]
     },
     '1': {
-      '-1': [[BACK, NE], [TOP, NW], [LEFT, NW]],
-      '0': [[TOP, W], [LEFT, N]],
-      '1': [[FRONT, NW], [TOP, SW], [LEFT, NE]]
+      '-1': [[B, NE], [U, NW], [L, NW]],
+      '0': [[U, W], [L, N]],
+      '1': [[F, NW], [U, SW], [L, NE]]
     }
   },
   '0': {
     '-1': {
-      '-1': [[BACK, S], [BOTTOM, W]],
-      '0': [[BOTTOM, C]],
-      '1': [[FRONT, S], [BOTTOM, N]]
+      '-1': [[B, S], [D, W]],
+      '0': [[D, C]],
+      '1': [[F, S], [D, N]]
     },
     '0': {
-      '-1': [[BACK, C]],
+      '-1': [[B, C]],
       '0': [],
-      '1': [[FRONT, C]]
+      '1': [[F, C]]
     },
     '1': {
-      '-1': [[BACK, N], [TOP, N]],
-      '0': [[TOP, C]],
-      '1': [[FRONT, N], [TOP, S]]
+      '-1': [[B, N], [U, N]],
+      '0': [[U, C]],
+      '1': [[F, N], [U, S]]
     }
   },
   '1': {
     '-1': {
-      '-1': [[BACK, SW], [BOTTOM, SE], [RIGHT, SE]],
-      '0': [[BOTTOM, E], [RIGHT, S]],
-      '1': [[FRONT, SE], [BOTTOM, NE], [RIGHT, SW]]
+      '-1': [[B, SW], [D, SE], [R, SE]],
+      '0': [[D, E], [R, S]],
+      '1': [[F, SE], [D, NE], [R, SW]]
     },
     '0': {
-      '-1': [[BACK, W], [RIGHT, E]],
-      '0': [[RIGHT, C]],
-      '1': [[FRONT, E], [RIGHT, W]]
+      '-1': [[B, W], [R, E]],
+      '0': [[R, C]],
+      '1': [[F, E], [R, W]]
     },
     '1': {
-      '-1': [[BACK, NW], [TOP, NE], [RIGHT, NE]],
-      '0': [[TOP, E], [RIGHT, N]],
-      '1': [[FRONT, NE], [TOP, SE], [RIGHT, NW]]
+      '-1': [[B, NW], [U, NE], [R, NE]],
+      '0': [[U, E], [R, N]],
+      '1': [[F, NE], [U, SE], [R, NW]]
     }
   }
 }
@@ -165,9 +165,17 @@ export class Cube {
   move (notation) {
     const mapping = {
       'F': () => this.rotate([0, 0, 1], true),
-      'B\'': () => this.rotate([0, 0, -1], true),
+      'F\'': () => this.rotate([0, 0, 1], false),
+      'B': () => this.rotate([0, 0, -1], true),
+      'B\'': () => this.rotate([0, 0, -1], false),
       'R': () => this.rotate([1, 0, 0], true),
-      'L\'': () => this.rotate([-1, 0, 0], true)
+      'R\'': () => this.rotate([1, 0, 0], false),
+      'L': () => this.rotate([-1, 0, 0], true),
+      'L\'': () => this.rotate([-1, 0, 0], false),
+      'U': () => this.rotate([0, 1, 0], true),
+      'U\'': () => this.rotate([0, 1, 0], false),
+      'D': () => this.rotate([0, -1, 0], true),
+      'D\'': () => this.rotate([0, -1, 0], false)
     }
     mapping[notation]()
   }
@@ -181,54 +189,41 @@ export class Cube {
 
   rotate (center, clockwise = true) {
     const axis = center.indexOf(1) + center.indexOf(-1) + 1
-    const prepareIndex = (i) => {
-      i.splice(axis, 0, center[axis])
+    // Fix y direction in right-handed coordinate system.
+    clockwise = center[1] !== 0 ? !clockwise : clockwise
+    // Fix directions whose faces are opposite to axis.
+    clockwise = center[axis] === 1 ? clockwise : !clockwise
 
-      if (clockwise) return
-      const [a, b] = [(axis + 1) % 3, (axis + 2) % 3]
-      ;[i[a], i[b]] = [i[b], i[a]]
-    }
-    const colorsAt = (i) => {
-      return this.blocks[(i[0] + 1) * 9 + (i[1] + 1) * 3 + (i[2] + 1)].colors
-    }
-    const cs = [[1, 1], [1, -1], [-1, -1], [-1, 1]] // corner indexes
-    const es = [[0, 1], [1, 0], [0, -1], [-1, 0]] // edge indexes
-    cs.forEach(prepareIndex)
-    es.forEach(prepareIndex)
+    let cs = [[1, 1], [1, -1], [-1, -1], [-1, 1]] // corner coords
+    let es = [[0, 1], [1, 0], [0, -1], [-1, 0]] // edge coords
+    const prepareCoord = coord => coord.splice(axis, 0, center[axis])
+    cs.forEach(prepareCoord); es.forEach(prepareCoord)
+    if (!clockwise) { cs = cs.reverse(); es = es.reverse() }
 
-    const shiftColor = (a, b, c, d) => {
+    // Rotate affected edge and corner blocks.
+    const rotateBlocks = (a, b, c, d) => {
       const set = (a, b) => { for (let i = 0; i < 6; i++) a[i] = [...b[i]] }
-      let tmp = []
-      if (clockwise) {
-        set(tmp, a); set(a, d); set(d, c); set(c, b); set(b, tmp)
-      } else {
-        set(tmp, a); set(a, b); set(b, c); set(c, d); set(d, tmp)
-      }
+      let tmp = []; set(tmp, a); set(a, d); set(d, c); set(c, b); set(b, tmp)
     }
-
+    const colorsAt = ([x, y, z]) => (
+      this.blocks[(x + 1) * 9 + (y + 1) * 3 + z + 1].colors
+    )
     let [a, b, c, d, e, f, g, h] = [
       colorsAt(cs[0]), colorsAt(cs[1]), colorsAt(cs[2]), colorsAt(cs[3]),
       colorsAt(es[0]), colorsAt(es[1]), colorsAt(es[2]), colorsAt(es[3])
     ]
+    rotateBlocks(a, b, c, d); rotateBlocks(e, f, g, h)
 
-    shiftColor(a, b, c, d); shiftColor(e, f, g, h)
-
-    const swap = [
-      [FRONT, TOP, BACK, BOTTOM],
-      [LEFT, BACK, RIGHT, FRONT],
-      [LEFT, TOP, RIGHT, BOTTOM]
-    ][axis]
-    const swapBlockFaces = (i) => {
-      const b = colorsAt(i)
-      if (clockwise) {
-        [b[swap[1]], b[swap[2]], b[swap[3]], b[swap[0]]] =
-        [b[swap[0]], b[swap[1]], b[swap[2]], b[swap[3]]]
-      } else {
-        [b[swap[0]], b[swap[1]], b[swap[2]], b[swap[3]]] =
-        [b[swap[1]], b[swap[2]], b[swap[3]], b[swap[0]]]
-      }
+    // Roatate all block faces with same rotation.
+    let swap = [
+      [[F, U, B, D], [L, F, R, B], [L, U, R, D]],
+      [[F, D, B, U], [F, L, B, R], [D, R, U, L]]
+    ][clockwise ? 0 : 1][axis]
+    const rotateFaces = (i) => {
+      const block = colorsAt(i)
+      ;[block[swap[1]], block[swap[2]], block[swap[3]], block[swap[0]]] =
+      [block[swap[0]], block[swap[1]], block[swap[2]], block[swap[3]]]
     }
-    cs.forEach(swapBlockFaces)
-    es.forEach(swapBlockFaces)
+    cs.forEach(rotateFaces); es.forEach(rotateFaces)
   }
 }
