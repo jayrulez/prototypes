cf2b3e18e2e918bbb96e22c2dd12a93a65f614e6
Yifeng Wang <i@ewind.us>
2018-08-14 11:27:28 +0800

    refactor: simplify data structure

--- gl/cube/cube.js
+++ gl/cube/cube.js
@@ -11,110 +11,78 @@ const COLORS = {
   YELLOW: [1.0, 1.0, 0.0, 1.0],
   GREY: [0.0, 0.0, 0.0, 0.5]
 }
-
 const [F, B, U, D, R, L] = [0, 1, 2, 3, 4, 5]
-const [NW, N, NE, W, C, E, SW, S, SE] = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-
 const FACE_MAPPING = {
   '-1': {
-    '-1': {
-      '-1': [[B, SE], [D, SW], [L, SW]],
-      '0': [[D, W], [L, S]],
-      '1': [[F, SW], [D, NW], [L, SE]]
-    },
-    '0': {
-      '-1': [[B, E], [L, W]],
-      '0': [[L, C]],
-      '1': [[F, W], [L, E]]
-    },
-    '1': {
-      '-1': [[B, NE], [U, NW], [L, NW]],
-      '0': [[U, W], [L, N]],
-      '1': [[F, NW], [U, SW], [L, NE]]
-    }
+    '-1': { '-1': [B, D, L], '0': [D, L], '1': [F, D, L] },
+    '0': { '-1': [B, L], '0': [L], '1': [F, L] },
+    '1': { '-1': [B, U, L], '0': [U, L], '1': [F, U, L] }
   },
   '0': {
-    '-1': {
-      '-1': [[B, S], [D, W]],
-      '0': [[D, C]],
-      '1': [[F, S], [D, N]]
-    },
-    '0': {
-      '-1': [[B, C]],
-      '0': [],
-      '1': [[F, C]]
-    },
-    '1': {
-      '-1': [[B, N], [U, N]],
-      '0': [[U, C]],
-      '1': [[F, N], [U, S]]
-    }
+    '-1': { '-1': [B, D], '0': [D], '1': [F, D] },
+    '0': { '-1': [B], '0': [], '1': [F] },
+    '1': { '-1': [B, U], '0': [U], '1': [F, U] }
   },
   '1': {
-    '-1': {
-      '-1': [[B, SW], [D, SE], [R, SE]],
-      '0': [[D, E], [R, S]],
-      '1': [[F, SE], [D, NE], [R, SW]]
-    },
-    '0': {
-      '-1': [[B, W], [R, E]],
-      '0': [[R, C]],
-      '1': [[F, E], [R, W]]
-    },
-    '1': {
-      '-1': [[B, NW], [U, NE], [R, NE]],
-      '0': [[U, E], [R, N]],
-      '1': [[F, NE], [U, SE], [R, NW]]
-    }
+    '-1': { '-1': [B, D, R], '0': [D, R], '1': [F, D, R] },
+    '0': { '-1': [B, R], '0': [R], '1': [F, R] },
+    '1': { '-1': [B, U, R], '0': [U, R], '1': [F, U, R] }
   }
 }
+const FACES = [
+  COLORS.BLUE, COLORS.GREEN, COLORS.YELLOW,
+  COLORS.WHITE, COLORS.RED, COLORS.ORANGE
+]
+const BASE_POSITIONS = [
+  // Front face
+  -1.0, -1.0, 1.0,
+  1.0, -1.0, 1.0,
+  1.0, 1.0, 1.0,
+  -1.0, 1.0, 1.0,
 
-const fillFace = (color, i) => [...Array(i)].map(() => color)
+  // Back face
+  -1.0, -1.0, -1.0,
+  -1.0, 1.0, -1.0,
+  1.0, 1.0, -1.0,
+  1.0, -1.0, -1.0,
 
-const positionFromCoords = (x, y, z) => {
+  // Top face
+  -1.0, 1.0, -1.0,
+  -1.0, 1.0, 1.0,
+  1.0, 1.0, 1.0,
+  1.0, 1.0, -1.0,
+
+  // Bottom face
+  -1.0, -1.0, -1.0,
+  1.0, -1.0, -1.0,
+  1.0, -1.0, 1.0,
+  -1.0, -1.0, 1.0,
+
+  // Right face
+  1.0, -1.0, -1.0,
+  1.0, 1.0, -1.0,
+  1.0, 1.0, 1.0,
+  1.0, -1.0, 1.0,
+
+  // Left face
+  -1.0, -1.0, -1.0,
+  -1.0, -1.0, 1.0,
+  -1.0, 1.0, 1.0,
+  -1.0, 1.0, -1.0
+]
+
+const positionsFromCoords = (x, y, z) => {
   const width = 1.9
   const margin = 0.05
-  const basePositions = [
-    // Front face
-    -1.0, -1.0, 1.0,
-    1.0, -1.0, 1.0,
-    1.0, 1.0, 1.0,
-    -1.0, 1.0, 1.0,
-
-    // Back face
-    -1.0, -1.0, -1.0,
-    -1.0, 1.0, -1.0,
-    1.0, 1.0, -1.0,
-    1.0, -1.0, -1.0,
-
-    // Top face
-    -1.0, 1.0, -1.0,
-    -1.0, 1.0, 1.0,
-    1.0, 1.0, 1.0,
-    1.0, 1.0, -1.0,
-
-    // Bottom face
-    -1.0, -1.0, -1.0,
-    1.0, -1.0, -1.0,
-    1.0, -1.0, 1.0,
-    -1.0, -1.0, 1.0,
-
-    // Right face
-    1.0, -1.0, -1.0,
-    1.0, 1.0, -1.0,
-    1.0, 1.0, 1.0,
-    1.0, -1.0, 1.0,
-
-    // Left face
-    -1.0, -1.0, -1.0,
-    -1.0, -1.0, 1.0,
-    -1.0, 1.0, 1.0,
-    -1.0, 1.0, -1.0
-  ]
   const diff = width + 2 * margin
-  const positions = basePositions
+  const positions = BASE_POSITIONS
     .map((v, i) => v * width / 2 + [x, y, z][i % 3] * diff)
   return positions
+}
+const colorsFromCoords = (x, y, z) => {
+  const colors = [...Array(6)].map(() => COLORS.GREY)
+  FACE_MAPPING[x][y][z].forEach(i => { colors[i] = FACES[i] })
+  return colors
 }
 
 export class Cube {
@@ -122,40 +90,17 @@ export class Cube {
     this.gl = canvas.getContext('webgl')
     this.programInfo = initProgram(this.gl)
     this.gl.useProgram(this.programInfo.program)
-
     this.initBlocks()
   }
 
-  getBlockColors (x, y, z) {
-    const colors = fillFace(COLORS.GREY, 6)
-    FACE_MAPPING[x][y][z].forEach(([face, grid]) => {
-      colors[face] = this.faces[face][grid]
-    })
-    return colors
-  }
-
-  getBlockPositions (x, y, z) {
-    // TODO apply transform for specific block positions.
-    return positionFromCoords(x, y, z)
-  }
-
   initBlocks () {
-    this.faces = [
-      fillFace(COLORS.BLUE, 9),
-      fillFace(COLORS.GREEN, 9),
-      fillFace(COLORS.YELLOW, 9),
-      fillFace(COLORS.WHITE, 9),
-      fillFace(COLORS.RED, 9),
-      fillFace(COLORS.ORANGE, 9)
-    ]
-
     this.blocks = []
     for (let x = -1; x <= 1; x++) {
       for (let y = -1; y <= 1; y++) {
         for (let z = -1; z <= 1; z++) {
           this.blocks.push({
-            positions: this.getBlockPositions(x, y, z),
-            colors: this.getBlockColors(x, y, z)
+            positions: positionsFromCoords(x, y, z),
+            colors: colorsFromCoords(x, y, z)
           })
         }
       }
@@ -178,6 +123,7 @@ export class Cube {
       'D\'': () => this.rotate([0, -1, 0], false)
     }
     mapping[notation]()
+    return this
   }
 
   render (delta = 0) {
@@ -201,21 +147,17 @@ export class Cube {
     if (!clockwise) { cs = cs.reverse(); es = es.reverse() }
 
     // Rotate affected edge and corner blocks.
-    const rotateBlocks = (a, b, c, d) => {
-      const set = (a, b) => { for (let i = 0; i < 6; i++) a[i] = [...b[i]] }
-      let tmp = []; set(tmp, a); set(a, d); set(d, c); set(c, b); set(b, tmp)
+    const rotateBlocks = ([a, b, c, d]) => {
+      const set = (a, b) => { for (let i = 0; i < 6; i++) a[i] = b[i] }
+      const tmp = []; set(tmp, a); set(a, d); set(d, c); set(c, b); set(b, tmp)
     }
     const colorsAt = ([x, y, z]) => (
       this.blocks[(x + 1) * 9 + (y + 1) * 3 + z + 1].colors
     )
-    let [a, b, c, d, e, f, g, h] = [
-      colorsAt(cs[0]), colorsAt(cs[1]), colorsAt(cs[2]), colorsAt(cs[3]),
-      colorsAt(es[0]), colorsAt(es[1]), colorsAt(es[2]), colorsAt(es[3])
-    ]
-    rotateBlocks(a, b, c, d); rotateBlocks(e, f, g, h)
+    rotateBlocks(cs.map(colorsAt)); rotateBlocks(es.map(colorsAt))
 
     // Roatate all block faces with same rotation.
-    let swap = [
+    const swap = [
       [[F, U, B, D], [L, F, R, B], [L, U, R, D]],
       [[F, D, B, U], [F, L, B, R], [D, R, U, L]]
     ][clockwise ? 0 : 1][axis]
@@ -225,5 +167,6 @@ export class Cube {
       [block[swap[0]], block[swap[1]], block[swap[2]], block[swap[3]]]
     }
     cs.forEach(rotateFaces); es.forEach(rotateFaces)
+    return this
   }
 }
