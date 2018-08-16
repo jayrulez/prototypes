c47981c9e4cb9aeafffc0650175064f52231f1c6
Yifeng Wang <i@ewind.us>
2018-08-16 10:14:38 +0800

    feat: handle bottom edge moves

--- gl/cube/solver.js
+++ gl/cube/solver.js
@@ -1,5 +1,5 @@
 import {
-  EDGE_COORDS, BOTTOM_COLOR, INIT_BLOCKS
+  D, U, EDGE_COORDS, BOTTOM_COLOR, INIT_BLOCKS, COLORS
 } from './consts'
 
 const base = INIT_BLOCKS() // base cube blocks
@@ -17,11 +17,89 @@ const getLostEdgeCoords = cube => EDGE_COORDS.filter(coord => {
   )
 })
 
-const solveCrossEdge = (cube, edgeCoord) => {
-  const edgeY = edgeCoord[1]
-  if (edgeY === -1) {
-    // Bottom layer
-  } else if (edgeY === 0) {
+const getTopFaceMove = ([x0, y0, z0], [x1, y1, z1]) => {
+  const offsetMapping = {
+    '0': { '1': { '1': 0, '-1': 2 } },
+    '-1': { '1': { '0': 3 } },
+    '1': { '1': { '0': 1 } }
+  }
+  const fromOffset = offsetMapping[x0][y0][z0]
+  const toOffset = offsetMapping[x1][y1][z1]
+  const moveMapping = {
+    '-3': ["U'"],
+    '-2': ['U', 'U'],
+    '-1': ['U'],
+    '0': [],
+    '1': ["U'"],
+    '2': ['U', 'U'],
+    '3': ['U']
+  }
+  return moveMapping[toOffset - fromOffset]
+}
+
+const movesRelativeTo = (coord, moves) => {
+  const moveRelativeTo = ([x, y, z], move) => {
+    const mapping = {
+      '0': { '0': { '-1': 2, '1': 0 } },
+      '1': { '0': { '0': 3 }, '1': { '1': 0, '-1': 3 } },
+      '-1': { '0': { '0': 1 }, '1': { '1': 1, '-1': 2 } }
+    }
+    const base = ['F', 'L', 'B', 'R']
+    const baseIndex = base.indexOf(move.replace('\'', ''))
+    const offset = mapping[x][y][z]
+    if (baseIndex === -1) return move
+    let newNotation = base[(baseIndex + offset) % 4]
+    newNotation += move.includes('\'') ? '\'' : ''
+    return newNotation
+  }
+  return moves.map(move => moveRelativeTo(coord, move))
+}
+
+const centerByColor = (color) => {
+  return {
+    [COLORS.BLUE]: [0, 0, 1],
+    [COLORS.GREEN]: [0, 0, -1],
+    [COLORS.RED]: [1, 0, 0],
+    [COLORS.ORANGE]: [-1, 0, 0],
+    [COLORS.YELLOW]: [0, 1, 0],
+    [COLORS.WHITE]: [0, -1, 0]
+  }[color]
+}
+
+const topEdgeToBottom = (cube, edgeCoord) => {
+  const moves = []
+  const topEdge = cube.getBlock(edgeCoord)
+  if (topEdge.colors[U] === BOTTOM_COLOR) {
+    const targetColor = topEdge.colors.find(
+      c => c !== BOTTOM_COLOR && c !== COLORS.GREY
+    )
+    const toCenter = centerByColor(targetColor)
+    const toTopEdge = [toCenter[0], toCenter[1] + 1, toCenter[2]]
+    moves.push(...getTopFaceMove(edgeCoord, toTopEdge))
+    moves.push(...movesRelativeTo(toCenter, ['F', 'F']))
+  }
+
+  return moves
+}
+
+const solveCrossEdge = (cube, [x, y, z]) => {
+  const moves = []
+  if (y === -1) {
+    const centerCoord = [x, y + 1, z]
+    if (cube.getBlock([x, y, z]).colors[D] === BOTTOM_COLOR) {
+      const tmpMoves = movesRelativeTo(centerCoord, ['F', 'F'])
+      tmpMoves.forEach(m => cube.move(m))
+      moves.push(...tmpMoves)
+    } else {
+      const tmpMoves = movesRelativeTo(centerCoord, ["F'", 'R', 'F', "R'"])
+      tmpMoves.forEach(m => cube.move(m))
+      moves.push(...tmpMoves)
+    }
+    const movesToBottom = topEdgeToBottom(cube, [x, y + 2, z])
+    movesToBottom.forEach(m => cube.move(m))
+    moves.push(...movesToBottom)
+    return moves
+  } else if (y === 0) {
     // Center layer
   } else {
     // Top layer
