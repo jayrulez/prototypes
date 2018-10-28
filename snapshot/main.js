bf32c7a164817ddf395548dc5152bb32a6170343
Yifeng Wang <i@ewind.us>
2018-10-28 11:50:32 +0800

    feat: init hash chunk poc

--- snapshot/main.js
+++ snapshot/main.js
@@ -1,4 +1,24 @@
 import { Snapshot } from './snapshot'
 
+console.clear()
+
+window.state = {
+  id: 0,
+  name: 'root',
+  children: [
+    { id: 1, name: 'a', children: [] },
+    { id: 2, name: 'b', children: [] },
+    {
+      id: 3,
+      name: 'c',
+      children: [
+        { id: 4, name: 'd', children: [] },
+        { id: 5, name: 'e', children: [] }
+      ]
+    }
+  ]
+}
+
 const snapshot = new Snapshot()
-console.log(snapshot)
+window.snapshot = snapshot
+// snapshot.pushSync(window.state)
--- snapshot/snapshot.js
+++ snapshot/snapshot.js
@@ -1,9 +1,35 @@
+// FIXME
+const hashFunc = str => str
+  .split('').map(s => s.charCodeAt(0)).reduce((a, b) => a + b, 0)
+
+const hash2State = (hashNode, chunks) => {
+  const { hash, children } = hashNode
+  const stateNode = JSON.parse(chunks[hash])
+  stateNode.children = children.map(node => hash2State(node, chunks))
+  return stateNode
+}
+
+const state2Hash = (stateNode, chunks) => {
+  const sanitizedState = { ...stateNode }
+  delete sanitizedState.children
+  const chunk = JSON.stringify(sanitizedState)
+  const hash = hashFunc(chunk)
+  chunks[hash] = chunk
+  const hashNode = {
+    hash,
+    children: stateNode.children.map(node => state2Hash(node, chunks))
+  }
+  return hashNode
+}
+
 export class Snapshot {
   constructor (options = {}) {
     const { rules = [] } = options
-    this.$index = 0
-    this.$states = []
     this.rules = rules
+
+    this.$index = 0
+    this.$hashTries = []
+    this.$chunks = {}
   }
 
   get hasUndo () {
@@ -14,7 +40,21 @@ export class Snapshot {
     return false
   }
 
-  push () {}
+  get () {
+    const currentTrie = this.$hashTries[this.$index]
+    if (!currentTrie) return null
+
+    return hash2State(currentTrie, this.$chunks)
+  }
+
+  // @return Snapshot
+  pushSync (state) {
+    this.$hashTries.push(state2Hash(state, this.$chunks))
+    this.$index = this.$hashTries.length - 1
+  }
+
+  // @return Promise<Snapshot>
+  pushAsync () {}
 
   undo () {}
 
