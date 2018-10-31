a6e59e1d4a0ea3a753aa8672575c234c6246b365
Yifeng Wang <i@ewind.us>
2018-10-31 12:08:42 +0800

    test: add transform test poc

---
+++ snapshot/transform.test.js
@@ -0,0 +1,25 @@
+/* global test expect */
+import { state2Record, record2State } from './transform'
+
+test('transform between state and record', () => {
+  const state = {
+    id: 0,
+    name: 'root',
+    children: [
+      { id: 1, name: 'a', children: [] },
+      { id: 2, name: 'b', children: [] },
+      {
+        id: 3,
+        name: 'c',
+        children: [
+          { id: 4, name: 'd', children: [] },
+          { id: 5, name: 'e', children: [] }
+        ]
+      }
+    ]
+  }
+  const chunks = {}
+  const record = state2Record(state, chunks, [], false)
+  const resultState = record2State(record, chunks)
+  expect(resultState).toEqual(state)
+})
