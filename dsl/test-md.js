b9b2238652d5e29e604466d2559b10e2e89f7299
Yifeng Wang <i@ewind.us>
2018-12-01 12:51:27 +0800

    chore: add html test source

---
+++ dsl/test-md.js
@@ -0,0 +1,85 @@
+// HTML copied from OneNote
+// document.addEventListener('paste', e => { debugger })
+// copy(e.clipboardData.getData('text/html'))
+export const html = `
+<html xmlns:o="urn:schemas-microsoft-com:office:office"
+xmlns:dt="uuid:C2F41010-65B3-11d1-A29F-00AA00C14882"
+xmlns="http://www.w3.org/TR/REC-html40">
+
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<meta name=ProgId content=OneNote.File>
+<meta name=Generator content="Microsoft OneNote 15">
+</head>
+
+<body lang=zh-CN style='font-family:Calibri;font-size:11.0pt'>
+<!--StartFragment-->
+
+<p style='margin:0in;font-family:微软雅黑;font-size:11.0pt'><span style='font-weight:
+bold'>技术选型中的锤子与钉子</span></p>
+
+<p style='margin:0in;font-family:微软雅黑;font-size:11.0pt' lang=x-none>&nbsp;</p>
+
+<p style='margin:0in;font-size:11.0pt'><span style='font-family:微软雅黑'
+lang=zh-CN>刚刚学会使用锤子（某种新技术）的时候，热情会促使你拿着它到处寻找钉子。这时候你眼中的一颗螺丝，都像是个可以运用锤子的钉子。但是，螺丝明明只需要用螺丝刀来拧就行了呀</span><span
+style='font-family:Calibri' lang=x-none> :)</span></p>
+
+<p style='margin:0in;font-family:微软雅黑;font-size:11.0pt' lang=en-US>&nbsp;</p>
+
+<p style='margin:0in;font-size:11.0pt'><span style='font-family:微软雅黑'
+lang=zh-CN>本末倒置的思维模型</span><span style='font-family:Calibri' lang=x-none>:</span></p>
+
+<ol type=1 style='margin-left:.375in;direction:ltr;unicode-bidi:embed;
+ margin-top:0in;margin-bottom:0in;font-family:Calibri;font-size:11.0pt;
+ font-weight:normal;font-style:normal'>
+ <li value=1 style='margin-top:0;margin-bottom:0;vertical-align:middle'><span
+     style='font-family:微软雅黑;font-size:11.0pt;font-weight:normal;font-style:
+     normal;font-family:微软雅黑;font-size:11.0pt' lang=zh-CN>学会使用了大</span><span
+     style='font-family:微软雅黑;font-size:11.0pt;font-weight:normal;font-style:
+     normal;font-family:微软雅黑;font-size:11.0pt' lang=en-US> </span><span
+     style='font-family:Calibri;font-size:11.0pt;font-weight:normal;font-style:
+     normal;font-family:Calibri;font-size:11.0pt' lang=x-none>V </span><span
+     style='font-family:微软雅黑;font-size:11.0pt;font-weight:normal;font-style:
+     normal;font-family:微软雅黑;font-size:11.0pt' lang=zh-CN>推荐的锤子（新工具），想要找钉子（理想场景）</span></li>
+</ol>
+
+<ol type=1 style='margin-left:.375in;direction:ltr;unicode-bidi:embed;
+ margin-top:0in;margin-bottom:0in;font-family:微软雅黑;font-size:11.0pt;font-weight:
+ normal;font-style:normal'>
+ <li value=2 style='margin-top:0;margin-bottom:0;vertical-align:middle'><span
+     style='font-family:微软雅黑;font-size:11.0pt;font-weight:normal;font-style:
+     normal;font-family:微软雅黑;font-size:11.0pt'>看见一颗螺丝（实际场景），认为它可以用锤子砸（套用工具）</span></li>
+ <li style='margin-top:0;margin-bottom:0;vertical-align:middle'><span
+     style='font-family:微软雅黑;font-size:11.0pt'>使用锤子砸螺丝，并宣称它自己拧的是火箭里的螺丝，锤子比手拧的效率更高（其实螺丝刀更方便）</span></li>
+</ol>
+
+<p style='margin:0in;font-family:Calibri;font-size:11.0pt'>&nbsp;</p>
+
+<p style='margin:0in;font-size:11.0pt'><span style='font-family:微软雅黑'
+lang=zh-CN>更实际的思维模型</span><span style='font-family:Calibri' lang=x-none>:</span></p>
+
+<ol type=1 style='margin-left:.375in;direction:ltr;unicode-bidi:embed;
+ margin-top:0in;margin-bottom:0in;font-family:Calibri;font-size:11.0pt;
+ font-weight:normal;font-style:normal'>
+ <li value=1 style='margin-top:0;margin-bottom:0;vertical-align:middle'><span
+     style='font-family:微软雅黑;font-size:11.0pt;font-weight:normal;font-style:
+     normal;font-family:微软雅黑;font-size:11.0pt'>这里有一颗螺丝（实际场景），目前我们只能手拧（痛点）</span></li>
+ <li style='margin-top:0;margin-bottom:0;vertical-align:middle'><span
+     style='font-family:微软雅黑;font-size:11.0pt'>针对螺丝有螺口和螺纹的性质（定位问题），找到螺丝刀（技术选型）</span></li>
+ <li style='margin-top:0;margin-bottom:0;vertical-align:middle'><span
+     style='font-family:微软雅黑;font-size:11.0pt'>使用螺丝刀拧螺丝（落地技术）</span></li>
+</ol>
+
+<p style='margin:0in;font-family:微软雅黑;font-size:11.0pt' lang=en-US>&nbsp;</p>
+
+<p style='margin:0in;font-size:11.0pt'><span style='font-family:微软雅黑'
+lang=zh-CN>晋级答辩里我有一页</span><span style='font-family:微软雅黑' lang=en-US> </span><span
+style='font-family:Calibri' lang=x-none>PPT </span><span style='font-family:
+微软雅黑' lang=zh-CN>介绍的是通过简单的正则匹配实现的源码自动化修改，这实现了包体积</span><span style='font-family:
+微软雅黑' lang=en-US> 20% </span><span style='font-family:微软雅黑' lang=zh-CN>以上的降低。答辩时这个点受到了挑战，问题大致是选择这个方案的考量，以及还有哪些改进的空间。对于这个问题，从‘锤子和钉子’的角度出发，更‘高大上’的语法树解析就像是个社区里火热的锤子，看起来对解决这类问题有最高的通用性。但是，我很清楚我需要的源码改动具备很容易辨识的模式，并且也是一次性的替换，并不需要花费大量时间在通用性方案的定制上，只需要写个处理字符串的脚本把源码丢进去就足够了。事实上，这个改动只花费了我一个下午的时间就收获了足够满意的效果，这表明在分析清楚问题场景的前提下，最契合自己且效率最高的工具，并不一定必须最通用和逼格最高的。</span></p>
+
+<!--EndFragment-->
+</body>
+
+</html>
+`
