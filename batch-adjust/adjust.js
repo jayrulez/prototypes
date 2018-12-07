c495c44f01c474aaa1db3b6338b96341034638cd
Yifeng Wang <i@ewind.us>
2018-12-07 12:09:27 +0800

    chore: optimize offset naming

--- batch-adjust/adjust.js
+++ batch-adjust/adjust.js
@@ -1,14 +1,14 @@
-export const batchAdjust = (items = [], groupedItems = [], diff = 0) => {
-  if (diff === 0) return items
+export const batchAdjust = (items = [], groupedItems = [], offset = 0) => {
+  if (offset === 0) return items
 
   let upperBound, lowerBound
-  if (diff > 0) {
+  if (offset > 0) {
     const lastGrouped = groupedItems[groupedItems.length - 1]
-    upperBound = Math.min(items.length - 1, items.indexOf(lastGrouped) + diff)
+    upperBound = Math.min(items.length - 1, items.indexOf(lastGrouped) + offset)
     lowerBound = upperBound - (groupedItems.length - 1)
   } else {
     const firstGrouped = groupedItems[0]
-    lowerBound = Math.max(0, items.indexOf(firstGrouped) + diff)
+    lowerBound = Math.max(0, items.indexOf(firstGrouped) + offset)
     upperBound = lowerBound + (groupedItems.length - 1)
   }
   const ungroupedItems = items.filter(item => !groupedItems.includes(item))
@@ -27,12 +27,12 @@ export const batchAdjust = (items = [], groupedItems = [], diff = 0) => {
 }
 
 export const batchAdjustAffected = (
-  items = [], affectedItems = [], groupedItems = [], diff = 0
+  items = [], affectedItems = [], groupedItems = [], offset = 0
 ) => {
   const subItems = items.filter(item => affectedItems.includes(item))
   const affectedIndexes = subItems.map(item => items.indexOf(item))
 
-  const adjustedSubItems = batchAdjust(subItems, groupedItems, diff)
+  const adjustedSubItems = batchAdjust(subItems, groupedItems, offset)
   const results = [...items]
   affectedIndexes.forEach((affectedIndex, subItemIndex) => {
     results[affectedIndex] = adjustedSubItems[subItemIndex]
