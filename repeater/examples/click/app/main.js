cabb403f9491925489b251066fe1ada4ad352815
Yifeng Wang <i@ewind.us>
2019-01-07 11:00:31 +0800

    chore: update demo ui

--- repeater/examples/click/app/main.js
+++ repeater/examples/click/app/main.js
@@ -1,19 +1,11 @@
 import '../../../recorder'
 
-const $btn = document.getElementById('btn')
 const $content = document.getElementById('content')
 let count = 0
 
-$btn.onclick = () => {
+$content.onclick = () => {
   $content.innerHTML = String(count++)
 }
-
-// TMP disable "side effect" for mouse events
-/*
-$content.addEventListener('mouseenter', () => {
-  $content.innerHTML = String(count++)
-})
-*/
 
 document.addEventListener('keydown', () => {
   $content.innerHTML = String(count++)
