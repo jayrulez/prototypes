a845882ac9bc90811c76f4e0500c5b35afdea391
Yifeng Wang <i@ewind.us>
2019-01-10 10:42:18 +0800

    refactor: add command scaffold

--- repeater/bin/repeater.js
+++ repeater/bin/repeater.js
@@ -2,7 +2,7 @@
 const fs = require('fs')
 const { join } = require('path')
 const program = require('commander')
-const { getDefaultChromiumPath } = require('./utils')
+const { getDefaultChromiumPath, getActionByName } = require('./utils')
 const { run } = require('./puppeteer-runner')
 const pkg = require('../package.json')
 
@@ -11,6 +11,7 @@ const defaultPath = getDefaultChromiumPath()
 program
   .version(pkg.version)
   .usage('<name> [options]')
+  .option('--update', '', false)
   .option('--concurrency [concurrency]', 'Test runner concurrency', 4)
   .option('--chromium-path [path]', 'Chromium revision path', defaultPath)
   .parse(process.argv)
@@ -18,11 +19,17 @@ program
 // program.concurrency
 // program.chromiumPath
 const name = program.args[0]
-if (name.includes('.json')) {
-  // TODO error handling
-  const fileName = join(process.cwd(), name)
-  const log = JSON.parse(fs.readFileSync(fileName, 'utf8'))
-  run(log)
-} else {
-  console.log('Batch runner WIP')
+const action = getActionByName(name, program.update)
+switch (action) {
+  case 'not-found': {
+    console.log('Invalid test name!')
+    process.exitCode = 1
+    break
+  }
+  case 'test-single': {
+    const fileName = join(process.cwd(), name)
+    const log = JSON.parse(fs.readFileSync(fileName, 'utf8'))
+    run(log)
+    break
+  }
 }
--- repeater/bin/utils.js
+++ repeater/bin/utils.js
@@ -1,4 +1,30 @@
+const fs = require('fs')
+const { join } = require('path')
 const os = require('os')
+
+const getActionByJSON = (name, update) => {
+  const jsonPath = join(process.cwd(), name)
+  if (!fs.existsSync(jsonPath)) {
+    return 'not-found'
+  }
+
+  if (update) {
+    return 'update-single'
+  }
+
+  const screenshotPath = jsonPath.replace('.json', '.png')
+  return fs.existsSync(screenshotPath) ? 'update-single' : 'test-single'
+}
+
+const getActionByDir = (name, update) => {
+  // const dir = join(process.cwd(), name)
+  return 'todo'
+}
+
+const getActionByName = (name, update) => {
+  return name.includes('.json')
+    ? getActionByJSON(name, update) : getActionByDir(name, update)
+}
 
 const getDefaultChromiumPath = () => {
   return os.platform() === 'darwin'
@@ -7,5 +33,6 @@ const getDefaultChromiumPath = () => {
 }
 
 module.exports = {
+  getActionByName,
   getDefaultChromiumPath
 }
