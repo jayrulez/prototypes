8bd108a1adca1bdf8c914de3ecb3e722a245d9b8
Yifeng Wang <i@ewind.us>
2019-02-22 20:04:00 +0800

    chore: add mock data

--- hit-test/main.js
+++ hit-test/main.js
@@ -1,5 +1,10 @@
+const elements = [
+  { type: 'rect', name: 'a', width: 150, height: 50, left: 30, top: 0 },
+  { type: 'rect', name: 'b', width: 20, height: 50, left: 100, top: 20 }
+]
+
 document.getElementById('btn').onclick = () => {
   const $canvas = document.getElementById('canvas')
   const context = $canvas.getContext('2d')
-  console.log(context)
+  console.log(context, elements)
 }
