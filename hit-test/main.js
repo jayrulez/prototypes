c43269e7125a0ccd3084086503dfc8a78ff36765
Yifeng Wang <i@ewind.us>
2019-02-26 11:36:39 +0800

    chore: add image draw scaffold

--- hit-test/main.js
+++ hit-test/main.js
@@ -1,24 +1,41 @@
 import { getRandomColor, HitTester } from './hit-test'
+import brushImgSrc from './brush-test.png'
 
-const elements = [
-  { type: 'rect', name: 'a', width: 150, height: 50, x: 30, y: 0 },
-  { type: 'rect', name: 'b', width: 20, height: 50, x: 100, y: 20 }
-]
+const $img = document.getElementById('img-test')
+$img.onload = () => {
+  const elements = [
+    { type: 'rect', name: 'rect-a', width: 150, height: 50, x: 30, y: 0 },
+    { type: 'rect', name: 'rect-b', width: 60, height: 80, x: 100, y: 20 },
+    {
+      type: 'image',
+      name: 'brush',
+      width: $img.naturalWidth,
+      height: $img.naturalHeight,
+      x: 0,
+      y: 0,
+      $el: $img
+    }
+  ]
 
-const $canvas = document.getElementById('canvas')
-const context = $canvas.getContext('2d')
-elements.forEach(element => {
-  if (element.type === 'rect') {
-    context.fillStyle = getRandomColor()
+  const $canvas = document.getElementById('canvas')
+  const ctx = $canvas.getContext('2d')
+  elements.forEach(element => {
     const { x, y, width, height } = element
-    context.fillRect(x, y, width, height)
-  }
-})
 
-const hitTester = new HitTester($canvas.width, $canvas.height)
-hitTester.update(elements)
+    if (element.type === 'rect') {
+      ctx.fillStyle = getRandomColor()
+      ctx.fillRect(x, y, width, height)
+    } else if (element.type === 'image') {
+      ctx.drawImage(element.$el, x, y, width, height)
+    }
+  })
 
-$canvas.addEventListener('click', (e) => {
-  const result = hitTester.detect(e.layerX, e.layerY)
-  console.log(result)
-})
+  const hitTester = new HitTester($canvas.width, $canvas.height)
+  hitTester.update(elements)
+
+  $canvas.addEventListener('click', (e) => {
+    const result = hitTester.detect(e.layerX, e.layerY)
+    console.log(result)
+  })
+}
+$img.src = brushImgSrc
