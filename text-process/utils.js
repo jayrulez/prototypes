2e18fa7251645187b997731051c842dcb570b2a7
Yifeng Wang <i@ewind.us>
2019-03-14 10:44:00 +0800

    chore: add outline extract scaffold

--- text-process/utils.js
+++ text-process/utils.js
@@ -1,4 +1,22 @@
-const getPointHOC = (bitmap, height) => (x, y) => bitmap[y * height + x]
+const buildPointGetter = (bitmap, height) => (x, y) => bitmap[y * height + x]
+
+// point in polygon
+export const pip = (point, vs) => {
+  // https://github.com/substack/point-in-polygon
+  const [x, y] = point
+
+  let inside = false
+  for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+    const [xi, yi] = vs[i]
+    const [xj, yj] = vs[j]
+
+    const intersect = ((yi > y) !== (yj > y)) &&
+          (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
+    if (intersect) inside = !inside
+  }
+
+  return inside
+}
 
 export const parseBitmap = canvas => {
   const gl = canvas.getContext('webgl')
@@ -17,7 +35,40 @@ export const parseBitmap = canvas => {
   return { bitmap, width, height }
 }
 
+/*
+// extract outline starting from x and y
+const extractOutline = (bitmap, x, y) => {
+  const begin = [x, y]
+  let curr = begin
+  const seqs = [
+    [0, -1], // n
+    [1, -1], // ne
+    [1, 0], // e
+    [1, 1], // se
+    [0, 1], // s
+    [-1, 1], // sw
+    [-1, 0], // w
+    [-1, -1] // nw
+  ]
+  return
+  const outline = []
+  while (true) {
+    for (let i = 0; i < seqs.length; i++) {
+      outline.push()
+    }
+  }
+}
+*/
+
 export const getOutline = (bitmap, width, height) => {
-  const get = getPointHOC(bitmap, height)
-  get(0, 0)
+  const get = buildPointGetter(bitmap, height)
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (get(i, j)) {
+        console.log(i, j)
+        // extractOutline(bitmap, j, i)
+        return
+      }
+    }
+  }
 }
