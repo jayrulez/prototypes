f8b14daca080f28e313de281e1c4fee9bc8ec866
Yifeng Wang <i@ewind.us>
2019-04-03 13:54:25 +0800

    chore: inline basic shadow poc

---
+++ inactive/gl/shadow-by-projection-webgl/shadow-by-projection-webgl.js
@@ -0,0 +1,485 @@
+/* eslint-disable */
+/*
+ * For maximum modularity, we place everything within a single function that
+ * takes the canvas that it will need.
+ */
+((canvas) => {
+    /*
+     * This code does not really belong here: it should live
+     * in a separate library of matrix and transformation
+     * functions.  It is here only to show you how matrices
+     * can be used with GLSL.
+     *
+     * Based on the original glRotate reference:
+     *     https://www.khronos.org/registry/OpenGL-Refpages/es1.1/xhtml/glRotate.xml
+     */
+    let getRotationMatrix = (angle, x, y, z) => {
+        // In production code, this function should be associated
+        // with a matrix object with associated functions.
+        let axisLength = Math.sqrt((x * x) + (y * y) + (z * z));
+        let s = Math.sin(angle * Math.PI / 180.0);
+        let c = Math.cos(angle * Math.PI / 180.0);
+        let oneMinusC = 1.0 - c;
+
+        // Normalize the axis vector of rotation.
+        x /= axisLength;
+        y /= axisLength;
+        z /= axisLength;
+
+        // Now we can calculate the other terms.
+        // "2" for "squared."
+        let x2 = x * x;
+        let y2 = y * y;
+        let z2 = z * z;
+        let xy = x * y;
+        let yz = y * z;
+        let xz = x * z;
+        let xs = x * s;
+        let ys = y * s;
+        let zs = z * s;
+
+        // GL expects its matrices in column major order.
+        return [
+            (x2 * oneMinusC) + c,
+            (xy * oneMinusC) + zs,
+            (xz * oneMinusC) - ys,
+            0.0,
+
+            (xy * oneMinusC) - zs,
+            (y2 * oneMinusC) + c,
+            (yz * oneMinusC) + xs,
+            0.0,
+
+            (xz * oneMinusC) + ys,
+            (yz * oneMinusC) - xs,
+            (z2 * oneMinusC) + c,
+            0.0,
+
+            0.0,
+            0.0,
+            0.0,
+            1.0
+        ];
+    };
+
+    /*
+     * This is another function that really should reside in a
+     * separate library.  But, because the creation of that library
+     * is part of the student course work, we leave it here for
+     * later refactoring and adaptation by students.
+     */
+    let getFrustumMatrix = (left, right, bottom, top, zNear, zFar) => {
+        let width = right - left;
+        let height = top - bottom;
+        let depth = zFar - zNear;
+
+        return [
+            2.0 * zNear / width,
+            0.0,
+            0.0,
+            0.0,
+
+            0.0,
+            2.0 * zNear / height,
+            0.0,
+            0.0,
+
+            (right + left) / width,
+            (top + bottom) / height,
+            -(zFar + zNear) / depth,
+            -1.0,
+
+            0.0,
+            0.0,
+            -2 * zFar * zNear / depth,
+            0.0
+        ];
+    };
+
+    // Grab the WebGL rendering context.
+    let gl = GLSLUtilities.getGL(canvas);
+    if (!gl) {
+        alert("No WebGL context found...sorry.");
+
+        // No WebGL, no use going on...
+        return;
+    }
+
+    // Set up settings that will not change.  This is not "canned" into a
+    // utility function because these settings really can vary from program
+    // to program.
+    gl.enable(gl.DEPTH_TEST);
+    gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ZERO); // Actually no blend, but we put it here so we can play with it.
+
+    // Build the objects to display.  Note how each object may come with a
+    // rotation axis now.
+    let objectsToDraw = [
+        // We move our original triangles a bit to accommodate a new addition
+        // to the scene (yes, a translation will also do the trick, if it
+        // where implemented in this program).
+        {
+            vertices: [].concat(
+                [ -2.0, 0.0, 0.0 ],
+                [ -1.5, 0.0, -0.75 ],
+                [ -2.0, 0.5, 0.0 ]
+            ),
+            colors: [].concat(
+                [ 1.0, 0.0, 0.0 ],
+                [ 0.0, 1.0, 0.0 ],
+                [ 0.0, 0.0, 1.0 ]
+            ),
+            mode: gl.TRIANGLES,
+            axis: { x: 0.0, y: 1.0, z: 0.0 }
+        },
+
+        {
+            color: { r: 0.0, g: 1.0, b: 0 },
+            vertices: [].concat(
+                [ -1.75, 0.0, -0.5 ],
+                [ -1.25, 0.0, -0.5 ],
+                [ -1.75, 0.5, -0.5 ]
+            ),
+            mode: gl.TRIANGLES,
+            axis: { x: 0.0, y: -1.0, z: 0.0 }
+        },
+
+        {
+            color: { r: 0.0, g: 0.0, b: 1.0 },
+            vertices: [].concat(
+                [ -2.25, 0.0, 0.5 ],
+                [ -1.75, 0.0, 0.5 ],
+                [ -2.25, 0.5, 0.5 ]
+            ),
+            mode: gl.TRIANGLES,
+            axis: { x: 0.1, y: 1.0, z: 0.0 }
+        },
+
+        {
+            color: { r: 0.0, g: 0.0, b: 1.0 },
+            vertices: [].concat(
+                [ 1.0, -1.0, 0.75 ],
+                [ 1.0, -0.1, -1.0 ],
+                [ 2.0, -0.1, -1.0 ],
+                [ 2.0, -1.0, 0.75 ]
+            ),
+            mode: gl.LINE_LOOP,
+            axis: { x: 1.0, y: 0.0, z: 1.0 }
+        },
+
+        {
+            ground: true,
+            color: { r: 0.0, g: 1.0, b: 1.0 },
+            vertices: [].concat(
+                [ -4.0, -1.01,  8.0 ],
+                [ -4.0, -1.01, -8.0 ],
+                [  4.0, -1.01, -8.0 ],
+                [  4.0, -1.01,  8.0 ]
+            ),
+            mode: gl.TRIANGLE_FAN
+        },
+
+        // Reference rectangle for a "floor."
+        {
+            ground: true,
+            color: { r: 1.0, g: 1.0, b: 1.0 },
+            vertices: [].concat(
+                [ -4.0, -2.01,  8.0 ],
+                [ -4.0, -2.01, -8.0 ],
+                [  4.0, -2.01, -8.0 ],
+                [  4.0, -2.01,  8.0 ]
+            ),
+            mode: gl.TRIANGLE_FAN
+        },
+
+        {
+            color: { r: 0.0, g: 0.5, b: 0.0 },
+            vertices: Shapes.toRawLineArray(Shapes.icosahedron()),
+            mode: gl.LINES,
+            axis: { x: 0.0, y: 1.0, z: 1.0 }
+        },
+
+        // Something that would have been clipped before.
+        {
+            vertices: [].concat(
+                [ 3.0, 1.5, 0.0 ],
+                [ 2.0, -1.5, 0.0 ],
+                [ 4.0, -1.5, 0.0 ]
+            ),
+            colors: [].concat(
+                [ 1.0, 0.5, 0.0 ],
+                [ 0.0, 0.0, 0.5 ],
+                [ 0.5, 0.75, 0.5 ]
+            ),
+            mode: gl.TRIANGLES,
+            axis: { x: -0.5, y: 1.0, z: 1.0 }
+        },
+
+        // Show off the new shape.
+        {
+            vertices: Shapes.toRawTriangleArray(Shapes.cube()),
+            // 12 triangles in all.
+            colors: [].concat(
+                [ 1.0, 0.0, 0.0 ],
+                [ 1.0, 0.0, 0.0 ],
+                [ 1.0, 0.0, 0.0 ],
+                [ 1.0, 0.0, 0.0 ],
+                [ 1.0, 0.0, 0.0 ],
+                [ 1.0, 0.0, 0.0 ],
+                [ 0.0, 1.0, 0.0 ],
+                [ 0.0, 1.0, 0.0 ],
+                [ 0.0, 1.0, 0.0 ],
+                [ 0.0, 1.0, 0.0 ],
+                [ 0.0, 1.0, 0.0 ],
+                [ 0.0, 1.0, 0.0 ],
+                [ 0.0, 0.0, 1.0 ],
+                [ 0.0, 0.0, 1.0 ],
+                [ 0.0, 0.0, 1.0 ],
+                [ 0.0, 0.0, 1.0 ],
+                [ 0.0, 0.0, 1.0 ],
+                [ 0.0, 0.0, 1.0 ],
+                [ 1.0, 1.0, 0.0 ],
+                [ 1.0, 1.0, 0.0 ],
+                [ 1.0, 1.0, 0.0 ],
+                [ 1.0, 1.0, 0.0 ],
+                [ 1.0, 1.0, 0.0 ],
+                [ 1.0, 1.0, 0.0 ],
+                [ 1.0, 0.0, 1.0 ],
+                [ 1.0, 0.0, 1.0 ],
+                [ 1.0, 0.0, 1.0 ],
+                [ 1.0, 0.0, 1.0 ],
+                [ 1.0, 0.0, 1.0 ],
+                [ 1.0, 0.0, 1.0 ],
+                [ 0.0, 1.0, 1.0 ],
+                [ 0.0, 1.0, 1.0 ],
+                [ 0.0, 1.0, 1.0 ],
+                [ 0.0, 1.0, 1.0 ],
+                [ 0.0, 1.0, 1.0 ],
+                [ 0.0, 1.0, 1.0 ]
+            ),
+            mode: gl.TRIANGLES,
+            axis: { x: 1.0, y: 1.0, z: 1.0 }
+        }
+    ];
+
+    // Pass the vertices to WebGL.
+    objectsToDraw.forEach((objectToDraw) => {
+        objectToDraw.vertexBuffer = GLSLUtilities.initVertexBuffer(gl, objectToDraw.vertices);
+
+        if (!objectToDraw.colors) {
+            // If we have a single color, we expand that into an array
+            // of the same color over and over.
+            objectToDraw.colors = [];
+            for (let i = 0, maxi = objectToDraw.vertices.length / 3; i < maxi; i += 1) {
+                objectToDraw.colors = objectToDraw.colors.concat(
+                    objectToDraw.color.r,
+                    objectToDraw.color.g,
+                    objectToDraw.color.b
+                );
+            }
+        }
+
+        objectToDraw.colorBuffer = GLSLUtilities.initVertexBuffer(gl, objectToDraw.colors);
+    });
+
+    // Initialize the shaders.
+    let abort = false;
+    let shaderProgram = GLSLUtilities.initSimpleShaderProgram(
+        gl,
+        $("#vertex-shader").text(),
+        $("#fragment-shader").text(),
+
+        // Very cursory error-checking here...
+        (shader) => {
+            abort = true;
+            alert("Shader problem: " + gl.getShaderInfoLog(shader));
+        },
+
+        // Another simplistic error check: we don't even access the faulty
+        // shader program.
+        (shaderProgram) => {
+            abort = true;
+            alert("Could not link shaders...sorry.");
+        }
+    );
+
+    // Ehrmagerd, _two_ shader programs?!?
+    let shadowProgram = GLSLUtilities.initSimpleShaderProgram(
+        gl,
+        $("#shadow-vertex-shader").text(),
+        $("#shadow-fragment-shader").text(),
+
+        // Very cursory error-checking here...
+        (shader) => {
+            abort = true;
+            alert("Shader problem: " + gl.getShaderInfoLog(shader));
+        },
+
+        // Another simplistic error check: we don't even access the faulty
+        // shader program.
+        (shaderProgram) => {
+            abort = true;
+            alert("Could not link shaders...sorry.");
+        }
+    );
+
+    // If the abort variable is true here, we can't continue.
+    if (abort) {
+        alert("Fatal errors encountered; we cannot continue.");
+        return;
+    }
+
+    // Hold on to the important variables within the shaders.
+    shaderProgram.vertexPosition = gl.getAttribLocation(shaderProgram, "vertexPosition");
+    gl.enableVertexAttribArray(shaderProgram.vertexPosition);
+    shaderProgram.vertexColor = gl.getAttribLocation(shaderProgram, "vertexColor");
+    gl.enableVertexAttribArray(shaderProgram.vertexColor);
+
+    shadowProgram.vertexPosition = gl.getAttribLocation(shadowProgram, "vertexPosition");
+    gl.enableVertexAttribArray(shadowProgram.vertexPosition);
+
+    // Finally, we come to the typical setup for transformation matrices:
+    // model-view and projection, managed separately.
+    shaderProgram.modelViewMatrix = gl.getUniformLocation(shaderProgram, "modelViewMatrix");
+    shaderProgram.projectionMatrix = gl.getUniformLocation(shaderProgram, "projectionMatrix");
+
+    shadowProgram.modelViewMatrix = gl.getUniformLocation(shadowProgram, "modelViewMatrix");
+    shadowProgram.projectionMatrix = gl.getUniformLocation(shadowProgram, "projectionMatrix");
+
+    /*
+     * Displays an individual object, including a transformation that now varies
+     * for each object drawn.
+     */
+    let drawObject = (object, program) => {
+        // Set the varying colors, if the program has them.
+        if (program.vertexColor) {
+            gl.bindBuffer(gl.ARRAY_BUFFER, object.colorBuffer);
+            gl.vertexAttribPointer(program.vertexColor, 3, gl.FLOAT, false, 0, 0);
+        }
+
+        // Set up the model-view matrix, if an axis is included. If not, we
+        // specify the identity matrix.
+        let modelViewMatrixArray = object.axis ?
+                getRotationMatrix(currentRotation, object.axis.x, object.axis.y, object.axis.z) :
+                [1, 0, 0, 0, // N.B. In a full-fledged matrix library, the identity
+                 0, 1, 0, 0, //      matrix should be available as a function.
+                 0, 0, 1, 0,
+                 0, 0, 0, 1];
+        modelViewMatrixArray[14] = -15.0; // Hardcoded translation to fit viewing volume.
+
+        gl.uniformMatrix4fv(program.modelViewMatrix, gl.FALSE, new Float32Array(modelViewMatrixArray));
+
+        // Set the varying vertex coordinates.
+        gl.bindBuffer(gl.ARRAY_BUFFER, object.vertexBuffer);
+        gl.vertexAttribPointer(program.vertexPosition, 3, gl.FLOAT, false, 0, 0);
+        gl.drawArrays(object.mode, 0, object.vertices.length / 3);
+    };
+
+    /*
+     * Displays the scene.
+     */
+    let drawScene = () => {
+        // Clear the display.
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        // Display the objects---twice! We need two loops because we want to ensure that all of
+        // the shadows are drawn last. Plus we will eliminate objects meant to be the "ground."
+        gl.useProgram(shaderProgram);
+        gl.disable(gl.BLEND);
+        objectsToDraw.forEach((objectToDraw) => {
+            drawObject(objectToDraw, shaderProgram);
+        });
+
+        gl.useProgram(shadowProgram);
+        gl.enable(gl.BLEND);
+        objectsToDraw.filter((objectToDraw) => {
+            return !objectToDraw.ground;
+        }).forEach((objectToDraw) => {
+            drawObject(objectToDraw, shadowProgram);
+        });
+
+        // All done.
+        gl.flush();
+    };
+
+    // Because our canvas element will not change size (in this program),
+    // we can set up the projection matrix once, and leave it at that.
+    // Note how this finally allows us to "see" a greater coordinate range.
+    // We keep the vertical range fixed, but change the horizontal range
+    // according to the aspect ratio of the canvas.  We can also expand
+    // the z range now.
+    let projectionMatrixArray = new Float32Array(getFrustumMatrix(
+        -2 * (canvas.width / canvas.height),
+        2 * (canvas.width / canvas.height),
+        -2,
+        2,
+        12,
+        10000
+    ));
+
+    [shaderProgram, shadowProgram].forEach((program) => {
+        gl.useProgram(program);
+        gl.uniformMatrix4fv(program.projectionMatrix, gl.FALSE, projectionMatrixArray);
+    });
+
+    /*
+     * Animates the scene.
+     */
+    let animationActive = false;
+    let currentRotation = 0.0;
+    let previousTimestamp = null;
+
+    const FRAMES_PER_SECOND = 60;
+    const MILLISECONDS_PER_FRAME = 1000 / FRAMES_PER_SECOND;
+
+    const DEGREES_PER_MILLISECOND = 0.033;
+    const FULL_CIRCLE = 360.0;
+
+    let advanceScene = (timestamp) => {
+        // Check if the user has turned things off.
+        if (!animationActive) {
+            return;
+        }
+
+        // Initialize the timestamp.
+        if (!previousTimestamp) {
+            previousTimestamp = timestamp;
+            window.requestAnimationFrame(advanceScene);
+            return;
+        }
+
+        // Check if it's time to advance.
+        var progress = timestamp - previousTimestamp;
+        if (progress < MILLISECONDS_PER_FRAME) {
+            // Do nothing if it's too soon.
+            window.requestAnimationFrame(advanceScene);
+            return;
+        }
+
+        // All clear.
+        currentRotation += DEGREES_PER_MILLISECOND * progress;
+        drawScene();
+        if (currentRotation >= FULL_CIRCLE) {
+            currentRotation -= FULL_CIRCLE;
+        }
+
+        // Request the next frame.
+        previousTimestamp = timestamp;
+        window.requestAnimationFrame(advanceScene);
+    };
+
+    // Draw the initial scene.
+    drawScene();
+
+    // Set up the rotation toggle: clicking on the canvas does it.
+
+    canvas.addEventListener('click', () => {
+        animationActive = !animationActive;
+        if (animationActive) {
+            previousTimestamp = null;
+            window.requestAnimationFrame(advanceScene);
+        }
+    });
+
+})(document.getElementById("matrices-webgl"));
