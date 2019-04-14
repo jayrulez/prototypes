47919f41255ce07db7891fa19494edb65a4f1cde
Yifeng Wang <i@ewind.us>
2019-04-15 01:21:38 +0800

    feat: add text diffuse support

--- gunpla/beam/examples/text/text.js
+++ gunpla/beam/examples/text/text.js
@@ -4,19 +4,25 @@ import {
   ShaderTypes,
   PropTypes
 } from '../../src/index.js'
+import { create } from '../../src/utils/mat4.js'
 
 const vertexShader = `
 attribute vec4 pos;
-// attribute vec4 color;
+attribute vec4 normal;
 
 uniform mat4 viewMat;
 uniform mat4 projectionMat;
+uniform mat4 normalMat;
 
 varying highp vec4 vColor;
 
 void main() {
+  vec4 color = vec4(1, 0, 0, 1);
+  vec3 lightDir = vec3(-0.35, 0.35, 0.87);
+  vec3 normalDir = normalize(vec3(normalMat * normal));
+  float nDotL = max(dot(normalDir, lightDir), 0.0);
+  vColor = vec4(color.rgb * nDotL, color.a);
   gl_Position = projectionMat * viewMat * pos;
-  vColor = vec4(1, 0, 0, 1);
 }
 `
 
@@ -32,29 +38,35 @@ export class TextPlugin extends ShadePlugin {
   constructor () {
     super()
 
-    const { vec3, mat4 } = ShaderTypes
+    const { vec3, vec4, mat4 } = ShaderTypes
     this.programSchema.vertexShader = vertexShader
     this.programSchema.fragmentShader = fragmentShader
-    this.programSchema.attributes = { pos: vec3 }
+    this.programSchema.attributes = {
+      pos: vec3,
+      normal: vec4
+    }
     this.programSchema.uniforms = {
       viewMat: mat4,
-      projectionMat: mat4
+      projectionMat: mat4,
+      normalMat: mat4
     }
 
     const { attribute } = PropTypes
     this.propSchema = {
       pos: { type: attribute, n: 3 },
+      normal: { type: attribute, n: 3 },
       index: { type: attribute, index: true }
     }
   }
 
   propsByElement ({ props }) {
-    const { positions, indices } = props
-    return { pos: positions, index: indices }
+    const { positions, normals, indices } = props
+    return { pos: positions, normal: normals, index: indices }
   }
 
   propsByGlobals (globals) {
     return {
+      normalMat: create(),
       viewMat: globals.camera,
       projectionMat: globals.perspective
     }
