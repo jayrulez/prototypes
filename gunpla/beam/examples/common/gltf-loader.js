486fafb9b005df0382f54325ffe62b90603a5be3
Yifeng Wang <i@ewind.us>
2019-04-17 00:19:34 +0800

    feat: add gltf buffer slice poc

--- gunpla/beam/examples/common/gltf-loader.js
+++ gunpla/beam/examples/common/gltf-loader.js
@@ -2,9 +2,26 @@
 
 const fetchGLTF = src => fetch(src).then(resp => resp.json())
 
+const fetchBuffer = src => fetch(src).then(resp => resp.arrayBuffer())
+
 // const defined = field => field !== undefined && field !== null
 
-export const loadGLTF = src => new Promise((resolve) => {
+const initBufferInfo = (gltf, bufferName, accessorName) => {
+  const accessor = gltf.accessors[accessorName]
+  const bufferView = gltf.bufferViews[accessor.bufferView]
+  const buffer = gltf.buffers[bufferView.buffer]
+  return {
+    bufferName,
+    accessorName,
+    accessor,
+    bufferView,
+    buffer,
+    uri: buffer.uri,
+    data: null
+  }
+}
+
+export const loadGLTF = (src, basePath) => new Promise((resolve) => {
   fetchGLTF(src).then(gltf => {
     const mesh = gltf.meshes[0]
     const primitive = mesh.primitives[0]
@@ -14,21 +31,24 @@ export const loadGLTF = src => new Promise((resolve) => {
       // attributeName: 'POSITION', 'NORMAL', 'TEXCOORD_0'...
       // accessorName: 0, 1, 2...
       const accessorName = attributes[attributeName]
-      const accessor = gltf.accessors[accessorName]
-      const bufferView = gltf.bufferViews[accessor.bufferView]
-      const buffer = gltf.buffers[bufferView.buffer]
-      const { uri } = buffer
-      return {
-        attributeName,
-        accessorName,
-        accessor,
-        bufferView,
-        buffer,
-        uri
-      }
+      return initBufferInfo(gltf, attributeName, accessorName)
+    })
+    const indicesInfo = initBufferInfo(gltf, 'INDEX', primitive.indices)
+    const bufferPath = basePath + indicesInfo.uri
+
+    fetchBuffer(bufferPath).then(buffer => {
+      attributeInfos.forEach(attributeInfo => {
+        const { byteOffset, byteLength } = attributeInfo.bufferView
+        attributeInfo.data = buffer.slice(byteOffset, byteOffset + byteLength)
+      })
+      const { byteOffset, byteLength } = indicesInfo.bufferView
+      indicesInfo.data = buffer.slice(byteOffset, byteOffset + byteLength)
+      resolve({
+        attributeInfos,
+        indicesInfo
+      })
     })
 
-    console.log(attributeInfos)
     window.gltf = gltf
   })
 })
