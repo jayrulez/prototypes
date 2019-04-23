e6f601fb5886548100f954620a38bdb1f65be577
Yifeng Wang <i@ewind.us>
2019-04-23 21:49:57 +0800

    refactor: fragment diffuse shading

--- gunpla/beam/examples/lighting/text.js
+++ gunpla/beam/examples/lighting/text.js
@@ -7,30 +7,36 @@ import {
 import { create } from '../common/mat4.js'
 
 const vertexShader = `
+precision highp float;
+
 attribute vec4 pos;
 attribute vec4 normal;
 
 uniform mat4 viewMat;
 uniform mat4 projectionMat;
-uniform mat4 normalMat;
-uniform vec3 directionalLightDir;
 
-varying highp vec4 vColor;
+varying vec4 vNormal;
 
 void main() {
-  vec4 color = vec4(1, 0, 0, 1);
-  vec3 normalDir = normalize(vec3(normalMat * normal));
-  float nDotL = max(dot(normalDir, directionalLightDir), 0.0);
-  vColor = vec4(color.rgb * nDotL, color.a);
+  vNormal = normal;
   gl_Position = projectionMat * viewMat * pos;
 }
 `
 
 const fragmentShader = `
-varying highp vec4 vColor;
+precision highp float;
+
+uniform vec3 directionalLightDir;
+uniform mat4 normalMat;
+
+varying vec4 vNormal;
 
 void main() {
-  gl_FragColor = vColor;
+  vec4 color = vec4(1, 0, 0, 1);
+  vec3 normalDir = normalize(vec3(normalMat * vNormal));
+  float nDotL = max(dot(normalDir, directionalLightDir), 0.0);
+  vec4 finalColor = vec4(color.rgb * nDotL, color.a);
+  gl_FragColor = finalColor;
 }
 `
 
