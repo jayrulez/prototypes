c27cc8107613d9811b5e1a67764aa3272c0448e0
Yifeng Wang <i@ewind.us>
2019-04-25 07:43:40 +0800

    feat: add env image scaffold

--- gunpla/beam/examples/prefilter/plane.js
+++ gunpla/beam/examples/prefilter/plane.js
@@ -10,27 +10,24 @@ import {
 const push = (arr, x) => { arr[arr.length] = x }
 
 const vertexShader = `
-attribute vec4 pos;
+attribute vec2 pos;
 attribute vec2 texCoord;
 
 varying highp vec2 vTexCoord;
 
 void main() {
-  gl_Position = vec4(pos.xy, 0, 1);
+  gl_Position = vec4(pos, 0, 1);
   vTexCoord = texCoord;
 }
 `
 
 const fragmentShader = `
-// uniform sampler2D img0;
-// uniform sampler2D img1;
+uniform sampler2D img;
 varying highp vec2 vTexCoord;
 
 void main() {
-  // highp vec4 colorA = texture2D(img0, vTexCoord);
-  // highp vec4 colorB = texture2D(img1, vTexCoord);
-  // gl_FragColor = colorA * colorB;
-  gl_FragColor = vec4(1, 0, 0, 1);
+  highp vec4 colorA = texture2D(img, vTexCoord);
+  gl_FragColor = colorA;
 }
 `
 
@@ -38,31 +35,27 @@ export class PlanePlugin extends ShadePlugin {
   constructor () {
     super()
 
-    const { vec2, vec3 } = ShaderTypes
-    // const { vec2, vec3, mat4, sampler2D } = ShaderTypes
+    const { vec2, sampler2D } = ShaderTypes
     this.vertexShader = vertexShader
     this.fragmentShader = fragmentShader
     this.shaderSchema.attributes = {
-      pos: vec3,
+      pos: vec2,
       texCoord: vec2
     }
     this.shaderSchema.uniforms = {
-      // img0: sampler2D,
-      // img1: sampler2D
+      img: sampler2D
     }
 
-    // const { buffer, texture } = PropTypes
-    const { buffer } = PropTypes
+    const { buffer, texture } = PropTypes
     this.propSchema = {
       pos: { type: buffer, n: 2 },
       texCoord: { type: buffer, n: 2 },
-      index: { type: buffer, index: true }
-      // img0: { type: texture, unit: 0, flip: true },
-      // img1: { type: texture, unit: 1, flip: true }
+      index: { type: buffer, index: true },
+      img: { type: texture, unit: 0, flip: true }
     }
   }
 
-  propsByElement () {
+  propsByElement ({ state }) {
     const basePositions = [
       -1.0, -1.0,
       1.0, -1.0,
@@ -84,9 +77,8 @@ export class PlanePlugin extends ShadePlugin {
     return {
       pos,
       texCoord,
-      index: [0, 1, 2, 0, 2, 3]
-      // img0: state.img0,
-      // img1: state.img1
+      index: [0, 1, 2, 0, 2, 3],
+      img: state.img
     }
   }
 }
