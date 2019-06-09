d98f435bf1a98b57eef644302b5747bc2f45c9f6
Yifeng Wang <i@ewind.us>
2019-06-09 11:32:30 +0800

    feat: support sphere hit detect

--- rt/test.js
+++ rt/test.js
@@ -2,21 +2,32 @@ import { Vec3, Ray, fillPixelWrapper } from './utils.js'
 
 const canvas = document.getElementById('output')
 const ctx = canvas.getContext('2d')
-const [nx, ny] = [200, 100]
+const [nx, ny] = [200, 100].map(x => x / 1)
 canvas.width = nx
 canvas.height = ny
 const fillPixel = fillPixelWrapper(ny)
 
 const bottomLeft = new Vec3(-2, -1, -1)
-
 const horizontal = new Vec3(4, 0, 0)
 const vertical = new Vec3(0, 2, 0)
 const origin = new Vec3(0, 0, 0)
 
-const color = r => {
-  const unit = r.direction.toUnit()
-  const t = 0.5 * (unit.y + 1)
+const hitSphere = (center, radius, ray) => {
+  const { x, y, z } = ray.origin
+  const oc = new Vec3(x, y, z).substract(center)
+  const a = Vec3.dot(ray.direction, ray.direction)
+  const b = 2 * Vec3.dot(oc, ray.direction)
+  const c = Vec3.dot(oc, oc) - radius * radius
+  const discriminant = b * b - 4 * a * c
 
+  return discriminant > 0
+}
+
+const color = ray => {
+  if (hitSphere(new Vec3(0, 0, -1), 0.5, ray)) return new Vec3(1, 0, 0)
+
+  const unit = ray.direction.toUnit()
+  const t = 0.5 * (unit.y + 1)
   return new Vec3(1, 1, 1).scale(1 - t).add(new Vec3(0.5, 0.7, 1).scale(t))
 }
 
--- rt/utils.js
+++ rt/utils.js
@@ -15,9 +15,22 @@ export class Vec3 extends Float32Array {
   get y () { return this[1] }
   get z () { return this[2] }
 
+  static dot (a, b) {
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+  }
+
+  static len (a) {
+    return Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2])
+  }
+
   add (b) {
     const a = this
     return new Vec3(a[0] + b[0], a[1] + b[1], a[2] + b[2])
+  }
+
+  substract (b) {
+    const a = this
+    return new Vec3(a[0] - b[0], a[1] - b[1], a[2] - b[2])
   }
 
   scale (b) {
@@ -25,14 +38,9 @@ export class Vec3 extends Float32Array {
     return new Vec3(a[0] * b, a[1] * b, a[2] * b)
   }
 
-  len () {
-    const a = this
-    return Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2])
-  }
-
   toUnit () {
     const a = this
-    return a.scale(1 / a.len())
+    return a.scale(1 / Vec3.len(a))
   }
 }
 
