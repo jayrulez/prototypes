7c665fa758c2581b1efdd7abca138f48b487aeb3
Yifeng Wang <i@ewind.us>
2019-12-03 17:15:05 +0800

    chore: add cpp poc

---
+++ cpp/main.cpp
@@ -0,0 +1,30 @@
+#include <memory>
+#include <iostream>
+
+class Resource
+{
+public:
+    Resource()
+    {
+        std::cout << "res ctor\n";
+    }
+    Resource(const Resource &r)
+    {
+        std::cout << "res copy\n";
+    }
+    ~Resource()
+    {
+        std::cout << "res dtor\n";
+    }
+};
+
+Resource CreateResource()
+{
+    Resource res = Resource();
+    return res;
+}
+
+int main()
+{
+    Resource r1 = CreateResource();
+}
