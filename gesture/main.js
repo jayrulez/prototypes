e46811e72937b9390c3f3fd9ff0eaf86bc4a9c1b
Yifeng Wang <i@ewind.us>
2020-03-11 18:21:38 +0800

    feat: add mock element poc

--- gesture/main.js
+++ gesture/main.js
@@ -5,7 +5,6 @@ let left = 0
 let top = 0
 void new AlloyFinger(box, {
   pressMove: function (evt) {
-    console.log(evt)
     const { deltaX, deltaY } = evt
     left += deltaX
     top += deltaY
@@ -13,3 +12,76 @@ void new AlloyFinger(box, {
     box.style.top = `${top}px`
   }
 })
+
+const simplify = touches => {
+  const result = []
+  for (let i = 0; i < touches.length; i++) {
+    const t = touches[i]
+    result.push({ pageX: t.pageX, pageY: t.pageY })
+  }
+  return result
+}
+
+class MockElement {
+  constructor (id) {
+    this.left = 0
+    this.top = 0
+    this.el = document.getElementById(id)
+    const noop = () => {}
+    this.callbacks = {
+      touchstart: noop,
+      touchmove: noop,
+      touchend: noop,
+      touchcancel: noop
+    }
+
+    this.el.addEventListener('touchstart', e => {
+      const mockEvt = {
+        touches: simplify(e.touches),
+        changedTouches: simplify(e.changedTouches)
+      }
+      this.callbacks.touchstart(mockEvt)
+    }, false)
+    this.el.addEventListener('touchmove', e => {
+      const mockEvt = {
+        touches: simplify(e.touches),
+        changedTouches: simplify(e.changedTouches)
+      }
+      this.callbacks.touchmove(mockEvt)
+    }, false)
+    this.el.addEventListener('touchend', e => {
+      const mockEvt = {
+        touches: simplify(e.touches),
+        changedTouches: simplify(e.changedTouches)
+      }
+      this.callbacks.touchend(mockEvt)
+    }, false)
+    this.el.addEventListener('touchcancel', e => {
+      const mockEvt = {
+        touches: simplify(e.touches),
+        changedTouches: simplify(e.changedTouches)
+      }
+      this.callbacks.touchcancel(mockEvt)
+    }, false)
+  }
+  addEventListener (type, callback) {
+    this.callbacks[type] = callback
+  }
+  removeEventListener (type, callback) {
+    // TODO
+    console.log(type, callback)
+  }
+}
+
+const mockBox = new MockElement('mock-box')
+void new AlloyFinger(mockBox, {
+  pressMove: function (evt) {
+    const { deltaX, deltaY } = evt
+    mockBox.left += deltaX
+    mockBox.top += deltaY
+    mockBox.el.style.left = `${mockBox.left}px`
+    mockBox.el.style.top = `${mockBox.top}px`
+    // console.log(evt)
+  }
+})
+window.mockBox = mockBox
