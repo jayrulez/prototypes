c4898a8365455b4a5bdb752ca3fca7bfaa19f04a
Yifeng Wang <i@ewind.us>
2020-08-10 22:57:46 +0800

    feat: support markdown export

--- bib-test/test.js
+++ bib-test/test.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const { parseBib } = require('./bib-parser.js');
 
-
 const src = fs.readFileSync('./jshopl.bib').toString('utf-8');
 const entries = parseBib(src);
 
@@ -13,6 +12,10 @@ const getURL = (entry) => {
   return url;
 };
 
+const fieldsToMarks = (fields) => fields
+  .map(f => `* <a id="${f.id}">${f.url ? `[${f.title}](${f.url})` : f.title}</a>`)
+  .join('\n');
+
 const fields = entries.map(entry => ({
   id: entry.id,
   author: entry.author,
@@ -23,4 +26,6 @@ const fields = entries.map(entry => ({
 if (missingUrlCount) console.error('Missing URL count', missingUrlCount);
 
 // console.log(JSON.stringify(entries, null, 2));
-console.log(JSON.stringify(fields, null, 2));
+// console.log(JSON.stringify(fields, null, 2));
+const dist = '# 参考文献\n\n' + fieldsToMarks(fields);
+console.log(dist);
