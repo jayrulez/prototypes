9221e9bd9a642a49c97226e0146583530cb34a5b
Yifeng Wang <i@ewind.us>
2020-12-09 14:50:40 +0800

    chore: add struct member variable test

---
+++ cpp/test.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <cstdio>
+
+struct MyData {
+    std::string* name;
+    float count;
+};
+
+class Base {
+private:
+    MyData _myData;
+public:
+    Base() {
+        printf("Name is null? %d\n", _myData.name == nullptr);
+    }
+};
+
+int main() {
+    Base base;
+    return 0;
+}
