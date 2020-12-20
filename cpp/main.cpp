cd309e7412f29f7cb85f81ba150834fd8d9e09b5
Yifeng Wang <i@ewind.us>
2020-12-21 00:44:59 +0800

    chore: rule of three

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 #include <memory>
 
-class Resource {
+class Node {
  public:
-  Resource() { std::cout << "res ctor\n"; }
-  Resource(const Resource& r) { std::cout << "res copy\n"; }
-  ~Resource() { std::cout << "res dtor\n"; }
+  Node(int n) : num(n) { std::cout << "default ctor" << std::endl; }
+  Node(const Node& r) {
+    std::cout << "copy ctor" << std::endl;
+    num = r.num;  // manually implement
+  }
+  Node& operator=(const Node& r) {
+    std::cout << "copy assignment operator" << std::endl;
+    num = r.num;  // manually implement
+    return *this;
+  }
+  // Node(Node&& r) {
+  //   std::cout << "move ctor" << std::endl;
+  //   std::cout << "count from src is " << r.num << std::endl;
+  // }
+  ~Node() { std::cout << "default dtor " << num << std::endl; }
+  int num;
 };
 
-Resource CreateResource() {
-  Resource res = Resource();
-  return res;
+Node CreateNode() {
+  Node r1(1), r2(2);
+  r1.num = 123;
+  // auto r3 = std::move(res);
+  r2 = r1;
+  printf("%d %d\n", r1.num, r2.num);
+  return r2;
 }
 
 int main() {
-  Resource r1 = CreateResource();
+  Node r = CreateNode();
+  printf("before main return...\n");
 }
