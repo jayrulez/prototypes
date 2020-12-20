729cd5d69b3138442f6100535384aeb292b7a74e
Yifeng Wang <i@ewind.us>
2020-12-21 02:42:06 +0800

    chore: add leak poc

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,20 +1,36 @@
-#include <cstdio>
+#include <iostream>
+#include <memory>
 #include <string>
 
-struct MyData {
-  std::string* name;
-  float count;
-};
-
-class Base {
- private:
-  MyData _myData;
-
+class MyResource {
  public:
-  Base() { printf("Name is null? %d\n", _myData.name == nullptr); }
+  std::string name;
+  float count;
+  ~MyResource() { std::cout << "res dtor" << std::endl; }
 };
+
+class MyContainer {
+ public:
+  std::unique_ptr<MyResource> res;
+  // MyResource r0;
+  // MyResource* res;
+  MyContainer() : MyContainer("") {}
+  MyContainer(const std::string& name) {
+    res = std::make_unique<MyResource>();
+    // res = new MyResource();
+    res->name = name;
+    res->count = 0.0f;
+  }
+  ~MyContainer() { std::cout << "container dtor" << std::endl; }
+};
+
+void testResourceLeak() {
+  MyContainer c1;
+  MyContainer c2("hello");
+  std::cout << c2.res->name << std::endl;
+}
 
 int main() {
-  Base base;
-  return 0;
+  testResourceLeak();
+  std::cout << "return..." << std::endl;
 }
